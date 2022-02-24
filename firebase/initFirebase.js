7214018bd4c5d017af28a4ed5a8afdb416e6e3a6
Daniel Cukier <danicuki@gmail.com>
2022-02-23 22:23:57 -0300

    changed firebase config to .env

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -2,7 +2,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './firebase-config.json'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,7 +9,19 @@ import firebaseConfig from './firebase-config.json'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
+
+const firebaseConfig = {
+  "apiKey": process.env.FIREBASE_API_KEY,
+  "authDomain": process.env.FIREBASE_PROJECT_ID + ".firebaseapp.com",
+  "projectId": process.env.FIREBASE_PROJECT_ID,
+  "storageBucket": process.env.FIREBASE_PROJECT_ID + ".appspot.com",
+  "messagingSenderId": process.env.FIREBASE_SENDER_ID,
+  "appId": process.env.FIREBASE_APP_ID,
+  "measurementId": process.env.FIREBASE_MEASUREMENT_ID
+}
+
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const db = getFirestore();
 
