27ab1004e4b66130dc23398738b8cb3ba081d03e
Vitagliano <gabrielrvita@gmail.com>
2022-03-09 02:22:13 -0300

    Courses page has been structured

--- components/Card/index.js
+++ components/Card/index.js
@@ -3,31 +3,132 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import Button from '../../components/Button'
+import { ClockIcon } from '@heroicons/react/solid'
 
 export default function Card(props) {
-  const { title, img, desc, children, id } = props
+  const { title, img, desc, duration, difficulty, active, children, id } = props
   return (
     <>
-      <div className="mx-2 mb-8 rounded-lg shadow-xl lg:mb-0">
-        <div className="">
-          <Link href={'/courses/' + id}>
+      <div
+        className={`rounded-lg bg-white-200 shadow-xl dark:bg-black-400 ${
+          active ? '' : 'cursor-default select-none opacity-60 dark:opacity-50'
+        }`}
+      >
+        <div>
+          {active ? (
+            <Link href={'/courses/' + id}>
+              <img
+                src={img}
+                className="h-full w-full cursor-pointer rounded-tr-xl rounded-tl-xl"
+              />
+            </Link>
+          ) : (
             <img
               src={img}
               className="h-full w-full rounded-tr-xl rounded-tl-xl"
             />
-          </Link>
+          )}
         </div>
         <div className="bg-white">
+          <div className="flex items-center justify-between px-6 pt-4">
+            <div className="jus flex items-center text-black-300 dark:text-white-100">
+              <ClockIcon className="h-4 w-4 md:h-4 md:w-4" />
+              <p className="ml-2 mb-0 p-0 text-xs">
+                {duration ? duration : 'Em breve...'}
+              </p>
+            </div>
+            {difficulty === 'Iniciante' ? (
+              <div className="rounded-full bg-green-200 py-1.5 px-6">
+                <p className="text-xs font-bold text-green-500">{difficulty}</p>
+              </div>
+            ) : difficulty === 'Intermediário' ? (
+              <div className="rounded-full bg-yellow-200 py-1.5 px-6">
+                <p className="text-xs font-bold text-yellow-500">
+                  {difficulty}
+                </p>
+              </div>
+            ) : difficulty === 'Avançado' ? (
+              <div className="rounded-full bg-red-200 py-1.5 px-6">
+                <p className="text-xs font-bold text-red-500">{difficulty}</p>
+              </div>
+            ) : (
+              <div className="rounded-full bg-gray-200 py-1.5 px-6">
+                <p className="text-xs font-bold text-gray-500">{difficulty}</p>
+              </div>
+            )}
+          </div>
           <div className="p-4">
             <div className="flex items-center">
               <h2 className="text-lg font-semibold">
-                <Link href={'/courses/' + id}>{title}</Link>
+                {active ? (
+                  <Link href={'/courses/' + id}>
+                    {title.substring(0, 60) + '...'}
+                  </Link>
+                ) : (
+                  title.substring(0, 60) + '...'
+                )}
               </h2>
             </div>
-            <p className="mt-2 text-xs text-gray-600">{desc}</p>
-
-            <div className="flex items-center justify-between py-4">
-              <Button>Ver projeto</Button>
+            <p className="mt-2 text-xs text-gray-600">
+              {desc.substring(0, 120) + '...'}
+            </p>
+            <div className="mt-4 flex">
+              <div>
+                <p className="rounded bg-gray-200 px-2 py-1 text-xs text-gray-600">
+                  Tags
+                </p>
+              </div>
+              <div className="pl-2">
+                <p className="rounded bg-gray-200 py-1 px-2 text-xs text-gray-600">
+                  Tags
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center justify-between pt-6 pb-4">
+              <div className="flex-no-wrap flex items-center">
+                <div className="h-8 w-8 rounded-md bg-cover bg-center">
+                  <img
+                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_0.png"
+                    alt
+                    className="border-white h-full w-full overflow-hidden rounded-full border-2 object-cover shadow dark:border-gray-700"
+                  />
+                </div>
+                <div className="-ml-2 h-8 w-8 rounded-md bg-cover">
+                  <img
+                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_1.png"
+                    alt
+                    className="border-white h-full w-full overflow-hidden rounded-full border-2 object-cover shadow dark:border-gray-700"
+                  />
+                </div>
+                <div className="-ml-2 h-8 w-8 rounded-md bg-cover bg-center">
+                  <img
+                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_2.png"
+                    alt
+                    className="border-white h-full w-full overflow-hidden rounded-full border-2 object-cover shadow dark:border-gray-700"
+                  />
+                </div>
+                <div className="-ml-2 h-8 w-8 rounded-md bg-cover">
+                  <img
+                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_3.png"
+                    alt
+                    className="border-white h-full w-full overflow-hidden rounded-full border-2 object-cover object-center shadow dark:border-gray-700"
+                  />
+                </div>
+                <div className="-ml-2 h-8 w-8 rounded-md bg-cover">
+                  <img
+                    src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png"
+                    alt
+                    className="border-white h-full w-full overflow-hidden rounded-full border-2 object-cover object-center shadow dark:border-gray-700"
+                  />
+                </div>
+              </div>
+              {active ? (
+                <Link href={'/courses/' + id}>
+                  <Button>{active ? 'Ver projeto' : 'Em breve'}</Button>
+                </Link>
+              ) : (
+                <Button>{active ? 'Ver projeto' : 'Em breve'}</Button>
+              )}
             </div>
           </div>
         </div>
--- pages/courses/index.js
+++ pages/courses/index.js
@@ -17,18 +17,23 @@ function Courses({ allCourses }) {
       <Head>
         <title>Lista de Cursos: </title>
       </Head>
-      <div className="lg container flex justify-center px-6 pt-6 sm:px-6 md:flex md:px-6 lg:px-32">
-        <ul className="grid grid-flow-row grid-cols-3 gap-4 overflow-hidden">
+      <div className="lg container mt-24 flex flex-col justify-center px-6 pb-12 sm:mt-20 sm:px-6 md:mt-20 md:flex md:px-6 lg:mt-6 lg:px-32">
+        <h4 className="mb-6 text-2xl font-bold leading-tight text-black-300 dark:text-white-100">
+          Cursos
+        </h4>
+        <ul className="grid grid-flow-row grid-cols-1 gap-8 overflow-hidden sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3">
           {allCourses.map((c) => {
-            console.log('c', c)
             return (
               <>
                 <Card
+                  active={c?.active}
                   key={c?.id}
                   id={c?.id}
                   img={c?.image_url}
                   title={c?.title}
                   desc={c?.description}
+                  difficulty={c?.difficulty}
+                  duration={c?.duration}
                 />
               </>
             )
