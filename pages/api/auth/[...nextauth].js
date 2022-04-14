3cd03c630695a90a6e9c77b910d06a850e50e254
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-14 12:59:29 -0300

    feat: create callbacks to discord provider oauth using nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,4 +12,33 @@ export default NextAuth({
     }),
     // ...add more providers 
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      // Send properties to the client, like an access_token from a provider.
+      session.accessToken = token.accessToken
+      session.discord = token.discord
+      return session
+    },
+    async signIn({ user, account, profile, email, credentials }) {
+      const discord = {
+        id: profile.id,
+        username: `${profile.username}#${profile.discriminator}`,
+        verified: profile.verified,
+        email: profile.email,
+        locale: profile.locale,
+      };
+      if(user) {
+        account.discord = discord;
+      }
+      return true;
+    },
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if(account) {
+        token.discord = account.discord
+        token.accessToken = account.access_token;
+      }
+      return token;
+    }
+  }
 })
