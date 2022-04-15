00ec33a874b9eca73f67db25abdb54a692ea1027
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-14 22:42:38 -0300

    Refac: Improving get user Id using onAuthStateChanged and fixing minor bugs on saving or remaning connected or disconnect, also removing user discord info on disconnecting

--- components/Card/Discord/index.js
+++ components/Card/Discord/index.js
@@ -1,24 +1,49 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import Link from 'next/link'
 
 import { signIn, signOut, useSession } from 'next-auth/react'
 import Button from '../../Button'
 import { auth } from '../../../firebase/initFirebase';
 import { getUserFromFirestore, updateUserDiscordIdinFirestore } from '../../../lib/user';
+import { onAuthStateChanged } from 'firebase/auth';
+import { async } from '@firebase/util';
 
 export default function DiscordCard() {
   const { data: session } = useSession()
+  const [discordConnected, setDiscordConnected] = useState(false)
+
   useEffect(async() => {
-    if (auth.currentUser){
-      const user = await getUserFromFirestore(auth.currentUser)
-      if (user && !user.discord) {
-        await updateUserDiscordIdinFirestore(session.discord, auth.currentUser.uid)
+    onAuthStateChanged(auth, (async user => {
+      if (user){
+        const userSession = await getUserFromFirestore(user)
+        console.log(userSession)
+        console.log(session)
+        if (session && !userSession.discord) {
+          console.log(session)
+          updateUserDiscordIdinFirestore(session?.discord, auth.currentUser.uid)
+          setDiscordConnected(true)
+        }else if (userSession.discord) setDiscordConnected(true) 
+      }else {
+        console.log('user not logged')
+      }
+    }))
+    }, [session])
+
+    const disconnectDiscord = async () => {
+      if (auth.currentUser) {
+        const userSession = await getUserFromFirestore(auth.currentUser)
+        if (userSession.discord) {
+          signOut({redirect: false})
+          updateUserDiscordIdinFirestore(null, auth.currentUser.uid)
+          setDiscordConnected(false)
+        }
       }
     }
-    }, [auth.currentUser])
+
   return (
     <>
-      {!session && (
+    {console.log(discordConnected)}
+      {!discordConnected && (
         <div className="rounded-lg bg-white-100 shadow-xl dark:bg-black-200">
           <div className="flex">
             <div className="px-6 py-5">
@@ -36,20 +61,20 @@ export default function DiscordCard() {
           </div>
         </div>
       )}
-      {session && (
+      {discordConnected && (
         <>
-          <div className="max-w-sm rounded-lg bg-white-100 shadow-xl dark:bg-black-200">
+          <div className=" rounded-lg bg-white-100 shadow-xl dark:bg-black-200">
             <div className="flex">
               <div className="px-6 py-5">
                 <p className="text-base font-medium leading-none text-black-200 dark:text-gray-100">
-                  ✅ Discord Conectado
+                  ✅ {session?.discord.username} Conectado
                 </p>
                 <p className="pt-2 text-xs leading-5 text-gray-500 dark:text-gray-400">
-                  Tenha acesso aos canais supersecretos para em nosso grupo.
-                  Quando você tiver acesso, não deixe de dizer olá!
+                  Estamos ansiosos para te conhecer, venha dar uma olhada!
                 </p>
                 <div className="pt-4">
-                  <Button onClick={() => signOut({redirect: false})}>Desconectar Discord</Button>
+                <a className='cursor-pointer' onClick={() => disconnectDiscord()}>Desconectar</a>
+                  {/*<Button onClick={() => disconnectDiscord()}>Desconectar Discord</Button>*/}
                 </div>
               </div>
             </div>
