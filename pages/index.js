560d2eac15e0f63863bd2a689748ab6b97a7e7d1
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-18 11:23:38 -0300

    Feat: Cards to improve bootcamps marketing

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <Head>
         <title>web3dev Bootcamp Platform</title>
       </Head>
-      <main className="container flex flex-col md:flex-row mx-auto py-2 px-6 sm:px-6 md:px-6 lg:px-32 my-6 sm:my-6 md:my-6 lg:my-32 xl:py-0 justify-between">
+      <main className="container flex flex-col md:flex-row mx-auto py-2 px-6 sm:px-6 md:px-6 lg:px-32 my-6 sm:my-6 md:my-6 lg:my-16 xl:py-0 justify-between">
         <div className='flex justify-center md:w-1/3 items-center flex-col text-start'>
           <div>
             <div className='text-5xl font-bold'>
@@ -45,7 +45,36 @@ export default function Home() {
           />
         </div>
       </main>
-      
+      <footer>
+        <div className='flex text-center justify-between container mx-auto px-6 py-2 sm:px-6 md:px-6 lg:px-32 xl:py-0'>
+          <div className='p-1 bg-gradient-to-br w-44 from-green-400 to-teal-400 rounded-lg'>
+            <div className='rounded-lg w-full h-full bg-zinc-900 shadow-xl sm:p-1 md:p-3 lg:p-4'>
+             100% gratuito, sempre!
+            </div>
+          </div>
+          <div className='p-1 bg-gradient-to-br w-44 from-green-400 to-violet-500 rounded-lg'>
+            <div className='rounded-lg w-full h-full bg-zinc-900 shadow-xl sm:p-1 md:p-3 lg:p-4'>
+             Desenvolva um Smart Contract em apenas 1 semana
+            </div>
+          </div>
+          <div className='p-1 bg-gradient-to-br w-44 from-violet-500 to-purple-500 rounded-lg'>
+            <div className='rounded-lg w-full h-full bg-zinc-900 shadow-xl sm:p-1 md:p-3 lg:p-4'>
+             Aprendizado baseado em projetos
+            </div>
+          </div>
+          <div className='p-1 bg-gradient-to-br w-44 from-fuchsia-400 to-pink-500 rounded-lg'>
+            <div className='rounded-lg w-full h-full bg-zinc-900 shadow-xl sm:p-1 md:p-3 lg:p-4'>
+             Ganhe um NFT para cada projeto finalizado
+            </div>
+          </div>
+          <div className='p-1 bg-gradient-to-br w-44 from-rose-400 to-orange-500 rounded-lg'>
+            <div className='rounded-lg w-full h-full bg-zinc-900 shadow-xl sm:p-1 md:p-3 lg:p-4'>
+             Trabalhe como dev em projetos web3
+            </div>
+          </div>
+        </div>
+        <br />
+      </footer>
     </Layout>
   )
 }
