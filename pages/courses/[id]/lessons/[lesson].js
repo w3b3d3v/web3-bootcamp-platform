a516368f115579aa8aea9502b0b59bfcc35d5a69
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-19 16:44:36 -0300

    WIP: working on showing lessons dynamically

--- pages/courses/[id]/lessons/[lesson].js
+++ pages/courses/[id]/lessons/[lesson].js
@@ -1,5 +1,8 @@
+import ReactMarkdown from 'react-markdown';
 import Layout from '../../../../components/layout'
 import { withProtected } from '../../../../hooks/route'
+import { getCourse } from '../../../../lib/course';
+import { getAllCourses } from '../../../../lib/courses';
 
 function Lessons({ course }) {
   console.log(course) // course is still undefined since theres no server side props yet
@@ -11,7 +14,7 @@ function Lessons({ course }) {
             return (
               <div key={l?.section + l?.lesson}>
                 <h2>{l?.section}</h2>
-                <h3>{l?.lesson}</h3>
+                <h3>{l?.lesson.title}</h3>
                 <ReactMarkdown>{l?.markdown}</ReactMarkdown>
               </div>
             )
@@ -20,6 +23,27 @@ function Lessons({ course }) {
     </Layout>
   )
 }
+//export async function getStaticProps({ params }) {
+//  console.log(params)
+//  const course = await getCourse(params.id)
+//  return {
+//    props: {
+//      course,
+//    },
+//  }
+//}
+
+//export async function getStaticPaths() {
+//  const lessons = (await getAllCourses()).map((c) => c.active && c.sections ? (Object.values(c.sections).flat().map(lesson => /*Object.assign({}, {lesson:*/`/courses/${c.id}/lessons/${lesson.file}`/*})*/)  ): `/courses/${c.id}/lessons/Lesson_1_Welcome.md`).flat()
+//  const paths = (await getAllCourses()).map((c) => `/courses/${c.id}`)
+//  return {
+//    paths: [
+//      {params: {...paths}},
+//      {params: {lesson: {...lessons}}},
+//    ],
+//    fallback: false,
+//  }
+//}
 
 
 export default Lessons
