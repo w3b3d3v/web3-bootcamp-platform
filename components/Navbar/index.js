2d2d6f10d2ad14a227dd922bafc6459fe4df13bb
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-20 10:13:45 -0300

    Fix: my profile and logout links

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -203,13 +203,13 @@ export default function Navbar() {
                             <li className="cursor-pointer py-2 text-sm leading-3 tracking-normal text-black-300 hover:text-primary-300 focus:text-primary-300 focus:outline-none dark:text-white-100">
                               <div className="flex items-center">
                                 <Link href="/profile">
-                                  <span className="ml-2">Meu perfil</span>
+                                  <a className="ml-2 flex w-full items-start">Meu perfil</a>
                                 </Link>
                               </div>
                             </li>
 
                             <li className="mt-2 flex cursor-pointer items-center py-2 text-sm leading-3 tracking-normal text-black-300 hover:text-primary-300 focus:text-primary-300 focus:outline-none dark:text-white-100">
-                              <button className="ml-2" onClick={()=> {
+                              <button className="ml-2 flex w-full items-start hover:underline" onClick={()=> {
                                 signOut({redirect: false});
                                 logout();
                                 }}>
