b770a720d532590fc74060f716078129643aaec1
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-20 18:33:11 -0300

    Fix: import head component from next on signup page

--- pages/auth/signup/index.js
+++ pages/auth/signup/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import Head from 'next/head';
 import { useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { toast } from 'react-toastify'
