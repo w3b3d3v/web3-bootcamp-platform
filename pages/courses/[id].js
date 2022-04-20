1fc0de5e7a80ff508a0aa11a5281b43cc45fbf04
Daniel Cukier <danicuki@gmail.com>
2022-04-19 21:55:32 -0300

    refactoring the countdown

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -17,6 +17,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { getUserFromFirestore, registerUserInCohortInFirestore } from '../../lib/user';
 import { CalendarIcon } from '@heroicons/react/solid'
 import ICalendarLink from "react-icalendar-link";
+import countdown from "../../lib/utils/countdown";
 
 function Course({ course }) {
   if (!course.active) return <NotFound />
@@ -36,43 +37,18 @@ function Course({ course }) {
     setRegisterOnCohort(true)
   }
 
-  const countDownDate = () => new Date(user?.cohorts?.startDate.toDate()).getTime();
-
   useEffect(() => {
     if (document) {
       const interval = setInterval(function () {
-        const now = new Date().getTime();
-        const distance = countDownDate() - now;
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        if (user?.cohorts?.startDate.toDate() > new Date()) {
-          setTimeLeft(`${days}d ${hours}h ${minutes}m ${seconds}s`);
-        }
-        if (distance < 0) {
+        const ct = countdown(user?.cohorts?.startDate.toDate());
+        setTimeLeft(ct);
+        if (!ct) {
           clearInterval(interval);
-          setTimeLeft(null);
         }
       }, 1000);
     }
   })
 
-  const calendarFunction = () => {
-    //fetch('https://www.googleapis.com/calendar/v3/users/me/calendarList', {
-    //  method: 'GET',
-    //  headers: {
-    //    'Authorization': auth.currentUser.accessToken,
-    //    'Content-Type': 'application/json'
-    //  }
-    //}).catch(err => {
-    //  return console.log(err)
-    //})
-    //.then(res => {
-    //  console.log(res.json())
-    //})
-
-  }
   return (
     <Layout>
       <div className="container mx-auto px-6 py-2 sm:px-6 md:px-6 lg:px-32 xl:py-0">
@@ -145,53 +121,53 @@ function Course({ course }) {
         }
         {
           timeLeft == null &&
-        <>
-        <div className="container my-8">
-          <Tabs course={course} />
+          <>
+            <div className="container my-8">
+              <Tabs course={course} />
 
-          <div className="relative z-10 my-8 w-full rounded-lg bg-white-100 p-8 shadow-xl dark:bg-black-200">
-            {course?.sections &&
-              Object.keys(course?.sections).sort().map((section) => {
-                return (
-                  <div key={section}>
-                    <span className="mb-4 font-bold">
-                      {section?.replace('Section_', 'Sessão ')}
-                    </span>
-                    <ul className="mt-2 mb-8 flex flex-col">
-                       {course?.sections[section].map((lesson) => {
-                        return (
-                          <li
-                            key={lesson.title}
-                            className="mb-2 items-center rounded bg-white-200 px-2 py-2 dark:bg-black-300"
-                          >
-                            <div className="flex items-center">
-                              <div className="relative mr-2 flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full">
-                                <input
-                                  disabled
-                                  type="radio"
-                                  name="radio"
-                                  className="checkbox absolute h-full w-full appearance-none rounded-full border border-gray-400 checked:border-none focus:outline-none"
-                                />
-                                <div className="check-icon z-1 hidden h-full w-full rounded-full border-4 border-indigo-700" />
-                              </div>
-                              <Link href={`/courses/${course.id}/lessons/${lesson.file}`}>
-                                <a>
-                                  <p className="m-0 p-0">
-                                    {lesson.title}
-                                  </p>
-                                </a>
-                              </Link>
-                            </div>
-                          </li>
-                        )
-                      }).sort((a,b) => a-b)}
-                    </ul>
-                  </div>
-                )
-              })}
-          </div>
-        </div>
-        </>
+              <div className="relative z-10 my-8 w-full rounded-lg bg-white-100 p-8 shadow-xl dark:bg-black-200">
+                {course?.sections &&
+                  Object.keys(course?.sections).sort().map((section) => {
+                    return (
+                      <div key={section}>
+                        <span className="mb-4 font-bold">
+                          {section?.replace('Section_', 'Sessão ')}
+                        </span>
+                        <ul className="mt-2 mb-8 flex flex-col">
+                          {course?.sections[section].map((lesson) => {
+                            return (
+                              <li
+                                key={lesson.title}
+                                className="mb-2 items-center rounded bg-white-200 px-2 py-2 dark:bg-black-300"
+                              >
+                                <div className="flex items-center">
+                                  <div className="relative mr-2 flex h-4 w-4 flex-shrink-0 items-center justify-center rounded-full">
+                                    <input
+                                      disabled
+                                      type="radio"
+                                      name="radio"
+                                      className="checkbox absolute h-full w-full appearance-none rounded-full border border-gray-400 checked:border-none focus:outline-none"
+                                    />
+                                    <div className="check-icon z-1 hidden h-full w-full rounded-full border-4 border-indigo-700" />
+                                  </div>
+                                  <Link href={`/courses/${course.id}/lessons/${lesson.file}`}>
+                                    <a>
+                                      <p className="m-0 p-0">
+                                        {lesson.title}
+                                      </p>
+                                    </a>
+                                  </Link>
+                                </div>
+                              </li>
+                            )
+                          }).sort((a, b) => a - b)}
+                        </ul>
+                      </div>
+                    )
+                  })}
+              </div>
+            </div>
+          </>
         }
       </div>
     </Layout>
