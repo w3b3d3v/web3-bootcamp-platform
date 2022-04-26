2ca2ff672884796e5957ebc5075a82776b2c3c4a
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-26 15:35:39 -0300

    Feat: Render lessons markdown

--- pages/courses/[id]/lessons/[lesson].js
+++ pages/courses/[id]/lessons/[lesson].js
@@ -5,20 +5,21 @@ import { withProtected } from '../../../../hooks/route'
 import { getCourse } from '../../../../lib/course';
 import { getAllCourses } from '../../../../lib/courses';
 
-function Lessons({ course }) {
-  //console.log(course) // course is still undefined since theres no server side props yet
+function Lessons({ course, lesson }) {
   return (
     <Layout>
       <Head>
         <title>Lição - Bootcamp Web3Dev</title>
       </Head>
-      <div className="relative z-10 my-8 w-full rounded-lg bg-white-100 p-8 shadow-xl dark:bg-black-200">
+      <div className="container rounded-lg bg-white-100 shadow-xl dark:bg-black-200 w-2/3 mx-auto px-6 my-8 py-2 sm:px-2 md:px-4 lg:px-14 xl:py-0">
         {course &&
           course?.lessons.map((l) => {
             return (
+              l.lesson.includes(lesson) &&
               <div key={l?.section + l?.lesson}>
-                <h2>{l?.section}</h2>
+                <h2 className='py-4'>{l?.section?.replace('Section_', 'Sessão ')}</h2>
                 <h3>{l?.lesson.title}</h3>
+                <br />
                 <ReactMarkdown>{l?.markdown}</ReactMarkdown>
               </div>
             )
@@ -27,27 +28,25 @@ function Lessons({ course }) {
     </Layout>
   )
 }
-//export async function getStaticProps({ params }) {
-//  console.log(params)
-//  const course = await getCourse(params.id)
-//  return {
-//    props: {
-//      course,
-//    },
-//  }
-//}
+export async function getStaticProps({ params }) {
+  const course = await getCourse(params.id)
+  const lesson = params.lesson
+  return {
+    props: {
+      course,
+      lesson
+    },
+  }
+}
 
-//export async function getStaticPaths() {
-//  const lessons = (await getAllCourses()).map((c) => c.active && c.sections ? (Object.values(c.sections).flat().map(lesson => /*Object.assign({}, {lesson:*/`/courses/${c.id}/lessons/${lesson.file}`/*})*/)  ): `/courses/${c.id}/lessons/Lesson_1_Welcome.md`).flat()
-//  const paths = (await getAllCourses()).map((c) => `/courses/${c.id}`)
-//  return {
-//    paths: [
-//      {params: {...paths}},
-//      {params: {lesson: {...lessons}}},
-//    ],
-//    fallback: false,
-//  }
-//}
-
+export async function getStaticPaths() {
+  const lessons = (await getAllCourses()).map((c) => c.active && c.sections ? (Object.values(c.sections).flat().map(lesson => /*Object.assign({}, {lesson:*/`/courses/${c.id}/lessons/${lesson.file}`/*})*/)  ): `/courses/${c.id}/lessons/Lesson_1_Welcome.md`).flat()
+  return {
+    paths: [
+      ...lessons,
+    ],
+    fallback: false,
+  }
+}
 
 export default Lessons
