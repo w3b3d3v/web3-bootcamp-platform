aaf24018de7e465827430ebec8f087549c0ee047
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-04-29 20:20:36 -0300

    [6]: Feat > Show user submissions on a previously submitted lesson

--- pages/courses/[id]/lessons/[lesson].js
+++ pages/courses/[id]/lessons/[lesson].js
@@ -6,9 +6,25 @@ import Modal from '../../../../components/Modal';
 import { withProtected } from '../../../../hooks/route';
 import { getCourse } from '../../../../lib/course';
 import { getAllCourses } from '../../../../lib/courses';
-import { useState } from 'react';
-function Lessons({ course, lesson }) {
+import React, { useState, useEffect } from 'react';
+import { getLessonsSubmissions } from '../../../../lib/lessons';
+import { auth } from '../../../../firebase/initFirebase';
+
+function Lessons({ course, lesson, lessonSubmissions }) {
   const [open, setOpen] = useState(false);
+  const [disable, setDisable] = useState(false);
+  const [userSubmission, setUserSubmission] = useState();
+  const ref = React.createRef();
+
+  useEffect(async() => {
+    lessonSubmissions = await getLessonsSubmissions()
+    lessonSubmissions.map(item => {
+      if(item.lesson === lesson && item.user === auth.currentUser?.uid) {
+        setUserSubmission(item.content.value)
+        setDisable(true);
+      }
+    });
+  }, [lessonSubmissions, auth.currentUser, open]);
   return (
     <Layout>
       <Head>
@@ -20,16 +36,24 @@ function Lessons({ course, lesson }) {
             return (
               l.lesson.includes(lesson) &&
               <div key={l?.section + l?.lesson}>
-                {/*<h2 className='py-4'>{l?.section?.replace('Section_', 'Sessão ')}</h2>*/}
+                <h2 className='py-4'>{l?.section?.replace('Section_', 'Sessão ')}</h2>
                 <h3>{l?.lesson.title}</h3>
                 <br />
-                <ReactMarkdown children={l?.markdown}/>
+                <ReactMarkdown children={l?.markdown} />
                 <div className='flex justify-center'>
-                  <Button customClass='w-2/3 my-8 mx-auto' onClick={() => setOpen(true)}>Enviar lição</Button>
+                  {disable ?
+                  <div className='flex flex-col w-6/12 text-center'>
+                      <Button ref={ref} customClass='my-8 opacity-60 dark:opacity-50' disabled >Lição enviada</Button>
+                      <div className='text-white-200 border-solid border-2 border-gray-600 font-medium rounded-lg px-4 py-3 mb-3 text-sm'>{userSubmission}</div>
+                  </div>
+                    :
+                    <Button ref={ref} customClass='w-2/3 my-8 mx-auto' onClick={() => setOpen(true)} >Enviar lição</Button>
+                  }
+
                   {open &&
                     <Modal
                       openExternal={open}
-                      onClose={() => setOpen(false)}
+                      onClose={() => setOpen(false) }
                       lesson={lesson}
                       course={course}
                     />
@@ -45,10 +69,12 @@ function Lessons({ course, lesson }) {
 export async function getStaticProps({ params }) {
   const course = await getCourse(params.id);
   const lesson = params.lesson;
+  const lessonSubmissions = await getLessonsSubmissions();
   return {
     props: {
       course,
-      lesson
+      lesson,
+      lessonSubmissions
     },
   };
 }
