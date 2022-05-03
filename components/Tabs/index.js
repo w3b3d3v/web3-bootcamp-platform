b2e4d5d6e84f9059d549d97e78e40682848eeab0
Romulo Rodrigues <biorrodrigues@gmail.com>
2022-05-02 21:19:41 -0300

    [77]: Feat > Exhibit progression on tabs

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,35 +1,57 @@
-import React, { useState } from 'react'
-const Tabs = (course) => {
-  const getCourse = course?.course;
-  if(!getCourse?.sections) return null;
+import React, { useState, useEffect } from 'react';
+import { auth } from '../../firebase/initFirebase';
 
+export default function Tabs({ course, isLessonPage, lessonsSubmitted }) {
+  const getCourse = course;
+  if(!getCourse?.sections) return null;
   // list all the section as tabs
   const [activeTab, setActiveTab] = useState(
-    Object.keys(getCourse?.sections)[0]
+    Object.keys(getCourse?.sections).sort()
   );
-
   const toggle = (tab) => {
     if(activeTab !== tab) setActiveTab(tab);
   };
+  const checkSectionCompleted = (section) => {
+    if(lessonsSubmitted){
 
+      return lessonsSubmitted.map(item => item.section == section && item.completed == item.total).filter(Boolean)[0];
+    }
+  };
+  const checkCurrentSection = (section) => {
+    if(lessonsSubmitted){
+
+      if(lessonsSubmitted.find(item => item.completed < item.total).section == section) return 'bg-violet-600';
+    }
+  };
   return (
     <div className="flex flex-col">
       <div className="flex flex-row justify-between">
         {Object.keys(getCourse?.sections).sort().map((section) => {
           return (
-            <a href={`#${section}`} key={section} onClick={() => toggle(section)} className={`item px-4 font-bold py-2 hover:no-underline rounded shadow-lg text-black-100 dark:text-white-100 ${activeTab === section ? 'bg-gray-200   dark:bg-black-100 dark:text-primary-100' : 'bg-white-100 dark:bg-black-200'}`}
-            >
-              <div className={`item flex-grow px-4 font-bold py-2 rounded shadow-lg ${activeTab === section ? 'bg-gray-200   dark:bg-black-100 dark:text-primary-100' : 'bg-white-100 dark:bg-black-200'
-                }`}>
-                <p className="m-0 p-0">
-                  {section?.replace('Section_', 'Sessão ')}
-                </p>
-              </div>
-            </a>
+            <div className={'flex justify-between'} key={section}>
+              {isLessonPage ?
+                <div className={`item flex-grow px-4 font-bold py-2 hover:no-underline rounded shadow-lg text-black-100 dark:text-white-100  ${checkSectionCompleted(section) ? 'dark:bg-green-500 ' : checkCurrentSection(section)} `}
+                >
+                  <div className={`item flex-grow px-4 font-bold py-2 rounded shadow-lg ${checkSectionCompleted(section) ? 'dark:bg-green-500 ' : checkCurrentSection(section)}`}>
+                    <p className="m-0 p-0">
+                      {section?.replace('Section_', 'Sessão ')}
+                    </p>
+                  </div>
+                </div>
+                :
+                <a href={`#${!isLessonPage ? section : ''}`} onClick={() => toggle(section)} className={`item flex-grow px-4 font-bold py-2 hover:no-underline rounded shadow-lg text-black-100 dark:text-white-100 ${checkSectionCompleted(section) ? 'dark:bg-green-500 ' : checkCurrentSection(section)} `}
+                >
+                  <div className={`item flex-grow px-4 font-bold py-2 rounded shadow-lg ${checkSectionCompleted(section) ? 'dark:bg-green-500 ' : checkCurrentSection(section)}`}>
+                    <p className="m-0 p-0">
+                      {section?.replace('Section_', 'Sessão ')}
+                    </p>
+                  </div>
+                </a>
+              }
+            </div>
           );
         })}
       </div>
     </div>
   );
 };
-export default Tabs;
