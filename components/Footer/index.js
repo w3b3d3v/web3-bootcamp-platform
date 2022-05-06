6f727fd318c040131045d0039b6e17c42c210752
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-06 08:49:15 -0300

    Fix: target blank on footer link click

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,10 +6,10 @@ export default function Footer() {
   return (
     <div className="container flex justify-center mx-auto px-6 py-2 sm:px-6 md:px-6 lg:px-32 xl:py-0 ">
       <p className='text-black-100 dark:text-white-100'>
-        Desenvolvido com ❤️ pela <Link href='https://www.web3dev.com.br/'><a className='text-black-100 dark:text-white-100'>
+        Desenvolvido com ❤️ pela <Link href='https://www.web3dev.com.br/'><a target="_blank" className='text-black-100 dark:text-white-100'>
           web3dev{' '}
         </a></Link>
-        e inspirado pela <Link href='https://buildspace.so/'><a className='text-black-100 dark:text-white-100'>
+        e inspirado pela <Link href='https://buildspace.so/'><a target="_blank" className='text-black-100 dark:text-white-100'>
           buildspace{' '}
         </a></Link>
         ✨
