5ccd9ebbfb7aaa91499640ee71ad2e401fc20ff3
Daniel Cukier <danicuki@gmail.com>
2022-05-13 18:01:48 -0300

    improve stats layout

--- pages/admin/stats.js
+++ pages/admin/stats.js
@@ -11,9 +11,9 @@ function Stats() {
   const [lastLessons, setLastLessons] = useState([])
 
   useEffect(async () => {
-    setLessonsStats(await lessonsStatus());
-    setLastLessons(await lastLesson());
-  }, []);
+    setLessonsStats(await lessonsStatus())
+    setLastLessons(await lastLesson())
+  }, [])
 
   return (
     <Layout>
@@ -23,38 +23,43 @@ function Stats() {
       <main className="container mx-auto mt-16 px-6 py-2 sm:px-6 md:px-6 lg:px-32 xl:py-0">
         <div className=" mb-3 mt-6 bg-white-100 p-3 dark:bg-black-200">
           <ul>
-            {lessonsStats
-              .map((l) => {
-                return (
-                  <li key={l.session}>
-                    {l.session} => {l.submitted}
-                  </li>
-                )
-              })
-            }
+            {lessonsStats.map((l) => {
+              return (
+                <li key={l.session}>
+                  {l.session} => {l.submitted}
+                </li>
+              )
+            })}
           </ul>
           <hr />
-          <table>
-            <thead>
-              <tr>
-                <td>Nome</td>
-              </tr>
-            </thead>
-            <tbody>
+          <div className="table-fixed">
+            <div className="max-w-4/5">
               {lastLessons.map((l) => {
                 return (
-                  <tr key={l.id}>
-                    <td className="mx-auto mb-1">{l.createdAt && new Date(l.createdAt.seconds * 1000).toLocaleString()}</td>
-                    <td>{l.user.email}<br />{l.user.discord && l.user.discord.username}<br />{l.user.uid}</td>
-                    <td></td>
-                    <td></td>
-                    <td><img src={l.content.value} /> </td>
-                  </tr>
+                  <div key={l.id}>
+                    <div className="fl mx-auto mb-1">
+                      {l.createdAt &&
+                        new Date(l.createdAt.seconds * 1000).toLocaleString()}
+                    </div>
+                    <div>
+                      {l.user.email}
+                      <br />
+                      {l.user.discord && l.user.discord.username}
+                      <br />
+                      {l.user.uid}
+                    </div>
+                    <div className="max-h-60">
+                      <img
+                        className="max-h-60 object-cover"
+                        src={l.content.value}
+                      />{' '}
+                    </div>
+                    <hr />
+                  </div>
                 )
               })}
-            </tbody>
-
-          </table>
+            </div>
+          </div>
         </div>
       </main>
     </Layout>
