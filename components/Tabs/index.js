d09e6afb0e09982e846aa6554f8d6381aee1969a
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-16 11:39:18 -0300

    [110]: Refac > extract variable

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { auth } from '../../firebase/initFirebase';
+import useAuth from '../../hooks/useAuth';
 
-export default function Tabs({ course, isLessonPage, lessonsSubmitted }) {
+export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort }) {
   const getCourse = course;
+  const { user } = useAuth()
   if(!getCourse?.sections) return null;
   // list all the section as tabs
   const [activeTab, setActiveTab] = useState(
@@ -11,14 +12,43 @@ export default function Tabs({ course, isLessonPage, lessonsSubmitted }) {
   const toggle = (tab) => {
     if(activeTab !== tab) setActiveTab(tab);
   };
+  const checkLessons = () => {
+    const list = [];
+    const courseSectionsLength = {};
+    Object.keys(course?.sections).sort().map((section) => {
+      course?.sections[section].map((lesson) => {
+        courseSectionsLength[section] = courseSectionsLength[section] ? courseSectionsLength[section] + 1 : 1;
+        list.push({ section, ...lesson }
+        );
+      });
+    });
+    const userLessons = lessonsSubmitted.filter(item => item.user == user.uid);
+    const userLessonsSubmittedInCurrentCohort = userLessons.filter(item => item.cohort === cohort?.id);
+    const sectionsCompleted = userLessonsSubmittedInCurrentCohort.map(lesson => {
+      return list.map(item => (item.section == lesson.section && item.file == lesson.lesson && item));
+    }).map(item => item.filter(Boolean)).flat();
+    const sectionsCompletedInCurrentCohort = sectionsCompleted.map(item => item.section).reduce(function(obj, b) {
+      obj[b] = ++obj[b] || 1;
+      return obj;
+    }, {});
+    const sections = Object.keys(courseSectionsLength);
+    const completed = sections.map(section => {
+      return {
+        section,
+        completed: sectionsCompletedInCurrentCohort[section] ? sectionsCompletedInCurrentCohort[section] : 0,
+        total: courseSectionsLength[section]
+      };
+    });
+    return completed;
+  };
   const checkSectionCompleted = (section) => {
-    if(lessonsSubmitted && section) {
-      return lessonsSubmitted.map(item => item?.section == section && item?.completed == item?.total).filter(Boolean)[0];
+    if(checkLessons() && section) {
+      return checkLessons().map(item => item?.section == section && item?.completed == item?.total).filter(Boolean)[0];
     }
   };
   const checkCurrentSection = (section) => {
-    if(lessonsSubmitted) {
-      if(lessonsSubmitted.find(item => item.completed < item.total)?.section == section) return 'bg-violet-600';
+    if(checkLessons()) {
+      if(checkLessons().find(item => item.completed < item.total)?.section == section) return 'bg-violet-600';
     }
   };
   return (
