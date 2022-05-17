40b7a494a9d5d8a5eea35ee302d88dfc8b8ae3ed
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-17 09:29:51 -0300

    rebase with main and conflict resolve

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -91,15 +91,17 @@ function Course({ course, currentDate }) {
     if(userSubmitted.every(item => item === false)) counter++;
     return userSubmitted.some(item => item === true);
   };
-
+  const userIsRegisteredAndCohortIsFuture = () => {
+    return userIsRegisteredInCohort() && typeof timeLeft == 'string' && timeLeft?.split('d')[0] > 30
+  }
   const userIsNotRegisteredAndCohortIsOpen = () => {
     return (!user?.cohorts || user?.cohorts?.length == 0) || !(userIsRegisteredInCohort()) && (cohort?.endDate >= new Date(currentDate));
   };
   const userIsNotRegisteredAndCohortIsClosed = () => {
     return ((!user?.cohorts || user?.cohorts?.length == 0) || !userIsRegisteredInCohort()) && (cohort?.endDate <= new Date(currentDate));
   };
-  const userIsRegisteredAndCohortWillOpen = () => {
-    return (userIsRegisteredInCohort()) && (cohort?.startDate >= new Date(currentDate));
+  const userIsRegisteredAndCohortWillOpenSoon = () => {
+    return (userIsRegisteredInCohort()) && (cohort?.startDate >= new Date(currentDate)) && typeof timeLeft == 'string' && timeLeft?.split('d')[0] < 30;
   };
   const userIsRegisteredAndCohortIsOpen = () => {
     return !userHasAlreadyParticipatedInACohort() && (userIsRegisteredInCohort()) && (cohort?.startDate <= new Date(currentDate)) && (timeLeft == null && user?.cohorts?.map(cohort => cohort.course_id).includes(course.id));
@@ -145,6 +147,14 @@ function Course({ course, currentDate }) {
             </div>
           </div>
         }
+        {userIsRegisteredAndCohortIsFuture() &&
+          <div className="flex flex-col justify-center items-center p-2 lg:p-6 bg-gradient-to-r from-cyan-900 to-teal-500 rounded-lg lg:items-center mb-4">
+          <div className="flex flex-col w-3/4 justify-center items-center">
+            <p className='text-2xl text-center'>Você já está inscrito nesta turma, a data de abertura será surpresa e avisada com antecedência!
+            Enquanto isso, deleite-se em nosso discord, faça network, aprenda em grupo e descubra as incríveis oportunidades em Web3!</p>
+          </div>
+        </div>
+        }
         {userIsNotRegisteredAndCohortIsOpen() && !userHasAlreadyParticipatedInACohort() &&
           <>
             <button id={`signup-cohort`} onClick={() => registerUserInCohort()} className="flex item w-full justify-center p-6 bg-gradient-to-r from-green-400 to-violet-500 rounded-lg cursor-pointer">Inscreva-se agora &#x1F31F;</button>
@@ -153,7 +163,7 @@ function Course({ course, currentDate }) {
             </div>
           </>
         }
-        {userIsRegisteredAndCohortWillOpen() &&
+        {userIsRegisteredAndCohortWillOpenSoon() &&
           <>
             <div className="flex flex-col justify-center items-center p-2 lg:p-6 bg-gradient-to-r from-cyan-900 to-teal-500 rounded-lg lg:items-center mb-4">
               <div className="flex flex-col w-3/4 justify-center items-center">
