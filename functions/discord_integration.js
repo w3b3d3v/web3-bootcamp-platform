d7ed763b733d661315ef0d1a2a0eba0f19f27cdc
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-25 18:24:04 -0300

    [116]: Feat > Create isolated test to send email on mocked data

--- functions/discord_integration.js
+++ functions/discord_integration.js
@@ -4,11 +4,11 @@ const { Routes } = require('discord-api-types/v10')
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_KEY)
 
 async function addDiscordRole(user_id, role) {
-  if(!user_id) return new Promise(()=>{});
+  if (!user_id) return
   try {
     const guild_id = process.env.DISCORD_SERVER_ID
     return rest.put(Routes.guildMemberRole(guild_id, user_id, role))
-  } catch(error) {
+  } catch (error) {
     console.error(error)
   }
 }
