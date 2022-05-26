6c62b8709ccdb2578e2b500b5320a70e83033299
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-26 14:02:05 -0300

    [116]: Feat > Test primary and secondary styles on button component

--- components/Button/index.spec.js
+++ components/Button/index.spec.js
@@ -3,6 +3,12 @@
  */
 import { Button } from './index'
 import { render, screen } from '@testing-library/react'
+const primaryButton = 
+    `text-black-300 dark:text-white-100 font-medium border-transparent rounded-lg bg-primary-300 px-4 py-3 text-sm transition duration-150 ease-in-out `;
+
+  const secondaryButton = 
+    ` bg-transparent font-medium rounded-lg px-4 py-3 cursor-pointer border-2 border-primary-300 text-sm text-primary-300 transition duration-150 ease-in-out hover:border-primary-400 hover:text-primary-400`;
+
 describe('Button', () => {
   it('should render', () => {
     render(<Button />)
@@ -15,5 +21,13 @@ describe('Button', () => {
   it('should be disabled by props', () => {
     render(<Button disabled />)
     expect(screen.getByTestId('button')).toHaveProperty('disabled', true)
+  })
+  it('should have primary style', () => {
+    render(<Button style={'primary'} />)
+    expect(screen.getByTestId('button')).toHaveClass(primaryButton)
+  })
+  it('should have secondary style', () => {
+    render(<Button style={'secondary'} />)
+    expect(screen.getByTestId('button')).toHaveClass(secondaryButton)
   })
 })
