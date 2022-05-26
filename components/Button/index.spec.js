7cfb0bd773db520eba22b761730a54ac7bc74f37
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-25 22:53:42 -0300

    [116]: Feat > Create unit test to Button

---
+++ components/Button/index.spec.js
@@ -0,0 +1,17 @@
+
+import { Button } from './index'
+import { render, screen } from '@testing-library/react'
+describe('Button', () => {
+  it('should render', () => {
+    render(<Button />)
+    expect(screen.getByTestId('button')).toBeInTheDocument()
+  })
+  it('should use custom class', () => {
+    render(<Button customClass="custom" />)
+    expect(screen.getByTestId('button')).toHaveClass('custom')
+  })
+  it('should be disabled by props', () => {
+    render(<Button disabled />)
+    expect(screen.getByTestId('button')).toHaveProperty('disabled', true)
+  })
+})
