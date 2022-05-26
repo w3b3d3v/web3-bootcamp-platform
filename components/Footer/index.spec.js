c2fbae2a6d2653aaed99ebc6a0dd62397a290cbd
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-25 22:48:17 -0300

    [116]: Feat > Create unit test to Footer

---
+++ components/Footer/index.spec.js
@@ -0,0 +1,9 @@
+
+import Footer from './index'
+import { render, screen } from '@testing-library/react'
+describe('Footer', () => {
+  it('should render', () => {
+    render(<Footer />)
+    expect(screen.getByTestId('footer')).toBeInTheDocument();
+  })
+})
