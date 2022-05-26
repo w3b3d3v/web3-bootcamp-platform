6596c9c61b3f71218171782918915f13a20e9688
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-25 22:47:45 -0300

    [116]: Feat > Install jest as test suite for nextjs code

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    '<rootDir>/functions/',
+  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  transform: {
+    '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+}
