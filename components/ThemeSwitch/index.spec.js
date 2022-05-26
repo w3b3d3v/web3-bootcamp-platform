fe3aec4f3144a93e59ee684890f6517f2d4618d6
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-26 14:01:23 -0300

    [116]: Feat > Create test to theme start

--- components/ThemeSwitch/index.spec.js
+++ components/ThemeSwitch/index.spec.js
@@ -4,16 +4,14 @@
 import ThemeSwitch from './index'
 import { render, screen } from '@testing-library/react'
 
-const setTheme = jest.fn()
-
 describe('ThemeSwitch', () => {
-
   it('should render', () => {
     render(<ThemeSwitch />)
     expect(screen.getByTestId('theme-switch-button')).toBeInTheDocument();
   })
 
-  it('should change themes', async () => {
-    //yet to be done
+  it('should start with light theme', async () => {
+    render(<ThemeSwitch />)
+    expect(screen.getByTestId('sun-icon')).toBeInTheDocument();
   })
 })
