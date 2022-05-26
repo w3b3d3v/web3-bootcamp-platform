5627287234449f37e53629bf95bb2ffeedb7d4a1
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-25 22:16:11 -0300

    [116]: Fix > Format code removing semicolon

--- functions/test/index.test.js
+++ functions/test/index.test.js
@@ -1,10 +1,10 @@
-const test = require('firebase-functions-test')();
+const test = require('firebase-functions-test')()
 
-const { expect } = require('chai');
-const chai = require('chai');
-const { onCohortRegister } = require('../emails');
-const assert = chai.assert;
-const { addUserToRole } = require('../discord_integration');
+const { expect } = require('chai')
+const chai = require('chai')
+const { onCohortRegister } = require('../emails')
+const assert = chai.assert
+const { addUserToRole } = require('../discord_integration')
 
 describe('Email Deliveries', () => {
   const cohortSnap = test.firestore.makeDocumentSnapshot({
@@ -17,36 +17,36 @@ describe('Email Deliveries', () => {
     email_content: {
       subject: '🏕️ Seu primeiro Smart Contract na Ethereum',
     }
-  }, 'cohorts/lloRUGwzJAG4Lj2B3bmJ');
+  }, 'cohorts/lloRUGwzJAG4Lj2B3bmJ')
   const courseSnap = test.firestore.makeDocumentSnapshot({
     duration: "1 semana",
     title: "Construa um App com Solidity + Ethereum Smart Contract",
-  }, 'courses/Solidity_And_Smart_Contracts');
+  }, 'courses/Solidity_And_Smart_Contracts')
   const userSnap = test.firestore.makeDocumentSnapshot({
     discord: {
       id: '342174857057927169',
     }
   })
-  const email = `example+${(Math.random()*10).toFixed(1)}@yahoo.com`
+  const email = `example+${(Math.random() * 10).toFixed(1)}@yahoo.com`
 
   it('should email after cohort signup', async () => {
     const sendEmail = await onCohortRegister('on_cohort_signup.js', email, cohortSnap.data(), courseSnap.data())
-    expect(sendEmail.accepted[0].status).to.equal('sent');
+    expect(sendEmail.accepted[0].status).to.equal('sent')
   })
 
   it('should set email_deliveries to true after send email', async () => {
     const sendEmail = await onCohortRegister('on_cohort_signup.js', email, cohortSnap.data(), courseSnap.data())
-    expect(sendEmail.accepted[0].status).to.equal('sent');
+    expect(sendEmail.accepted[0].status).to.equal('sent')
     const cohortAfter = test.makeChange(cohortSnap, {
       email_deliveries: {
         signup: true
       }
     })
-    expect(cohortAfter.after.email_deliveries.signup).to.equal(true);
+    expect(cohortAfter.after.email_deliveries.signup).to.equal(true)
   })
 
   it('should add discord_role after cohort signup if discord is connected', async () => {
     const addUser = await addUserToRole(userSnap.data().discord.id, cohortSnap.data().discord_role)
-    expect(addUser.byteLength).to.equal(0);
+    expect(addUser.byteLength).to.equal(0)
   })
-});
+})
