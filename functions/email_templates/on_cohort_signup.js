4d9d6f1dc5e05a3742f36c7bca9b77f632b39681
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-05-27 16:21:17 -0300

    [82]: Refac > Email templates

--- functions/email_templates/on_cohort_signup.js
+++ functions/email_templates/on_cohort_signup.js
@@ -1,28 +1,25 @@
-function template(cohort, course) {
+function template(params) {
   return ( `
-      <!DOCTYPE html>Fala pessoal! danicuki da web3dev aqui.<div><br></div>
-      <div><b>Bem vindos</b> ao bootcamp "${course.title}".&nbsp; <a href="https://bootcamp.web3dev.com.br/courses/${cohort.course_id}">Esse é o link do bootcamp</a>. 
-      A live começa exatamente às 19h horário de Brasília no <a href="https://www.twitch.tv/web3dev">nosso Twitch</a>.
-  <div><br></div>
-  <div><b>Esse projeto é praticamente todo assíncrono.</b>&nbsp;O único momento ao vivo será nossa "live". Se não puder participar, não se preocipe. Ela também será gravada e disponibilizada para você assistir!</div>
-  <div><br></div>
-  <div><b>O material do bootcamp será aberto oficialmente as 19h, junto com a live <a href="https://bootcamp.web3dev.com.br/courses/${cohort.course_id}">aqui</a>&nbsp;</div>
-  <div>
-  <div><br></div>
-  <h3>Algumas informações importantes:</h3>
-  <div>1. Se você terminar o projeto em até ${course.duration}, você terá direito a receber um <b> NFT exclusivo </b>. Você também será adicionado ao cargo #formado no nosso Discord e terá acesso a vagas de trabalho web3 em nossos parceiros.</div>
-  <div><br></div>
-  <div>2. Esse é o único e-mail que eu vou mandar. O resto estará acontecendo no canal secreto "#⛺ | ${course.discord_channel}" que você terá acesso caso esteja inscrito no bootcamp. Para isso, você precisa "linkar" seu Discord na plataforma de bootcamp antes de começar o projeto!
-  </div>
-  <div><br></div>
-  <div>3. Por favor, não chama isso de "curso"! É um projeto, bootcamp, hackday, chama do que quiser. Só quero que você faça um trabalho legal com um pouco de guia :-).</div>
-  <div><br></div>
-  <div>Se tiver perguntas, mande no canal #chat-geral.</div>
-  <div><br></div>
-  <div>Estou animado para ver o projeto de todo mundo 💜💜💜.</div>
-  <div><br></div>
-  <div>danicuki</div>
-  </div>
+      <!DOCTYPE html>Fala builder 👷👷‍♀️!! danicuki da web3dev aqui.<br>
+      <br>
+      Sua inscrição foi feita no bootcamp "${params.course.title}".&nbsp; O bootcamp começa com a live de kickoff em ${params.cohort.kickoffStartDate}. Para acompanhar é só entrar no <a href="https://www.twitch.tv/web3dev">nosso Twitch</a>.<br>
+      <br>
+      <b>Esse projeto é praticamente todo assíncrono.</b>&nbsp;O único momento ao vivo será nossa live. Se não puder participar, não se preocupe. Ela também será gravada e disponibilizada para você assistir!<br>
+      <br>
+      <b>O material do bootcamp será aberto oficialmente às 19h, junto com a live <a href="https://bootcamp.web3dev.com.br/courses/${params.cohort.course_id}">aqui</a>&nbsp;<br>
+      <br>
+      <h3>Algumas informações importantes:</h3>
+      1. Se você terminar o projeto até ${params.cohort.endDate}, você terá direito a receber um <b>NFT exclusivo</b>. Vamos dar para você o cargo de #graduad@ no nosso Discord e terá acesso a vagas de trabalho web3 em nossos parceiros.<br>
+      <br>
+      2. Não se esqueça de conectar seu Discord na <a href="https://bootcamp.web3dev.com.br/courses/${params.cohort.course_id}">plataforma de bootcamps</a>, pois precisamos adicionar você no canal secreto "#⛺ | ${params.course.discord_channel}" da turma que fará parte deste bootcamp junto com você.<br>
+      <br>
+      3. Por favor, não chama isso de "curso"! É um projeto, bootcamp, hackday, chama do que quiser. Só quero que você faça um trabalho legal com um pouco de guia :-).<br>
+      <br>
+      Se tiver perguntas, mande no canal #chat-geral.<br>
+      <br>
+      Estou animado para ver o projeto de todo mundo 💜💜💜.<br>
+      <br>
+      danicuki
   ` )
 } 
 module.exports = template
