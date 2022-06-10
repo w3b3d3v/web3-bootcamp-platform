5de539a36a5be32d9cd7f7f18fe8b5b21fdc2870
Daniel Cukier <danicuki@gmail.com>
2022-06-10 14:41:06 -0300

    fix prettier and add contract JSON to repo

--- functions/mintNFT.js
+++ functions/mintNFT.js
@@ -1,5 +1,5 @@
-const { ethers } = require("ethers")
-const contractABI = require('../nfts/artifacts/contracts/BootcampNFTContract.sol/W3DBootcamp.json')
+const { ethers } = require('ethers')
+const contractABI = require('./W3DBootcampContract.json')
 const { sendEmail } = require('./emails')
 const contractAddress = '0xeA4aDb7A75a2452585204E8B411c64C2f3693845'
 
@@ -8,24 +8,23 @@ async function mint(cohort, nft_title, user) {
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
   const nftContract = new ethers.Contract(contractAddress, contractABI.abi, signer)
   await nftContract.mintCertificate(cohort.name, nft_title, user.wallet)
-  
-  nftContract.once("Transfer", async (a, wallet, id) => {
-    if (wallet !== user.wallet) return;
+ 
+  nftContract.once('Transfer', async (a, wallet, id) => {
+    if (wallet !== user.wallet) return
     const params = {
       cohort,
       course_title: nft_title,
       wallet_address: user.wallet,
       nft_contract: contractAddress,
       nft_id: id,
-    };
-    console.log("new id1 " + id + " for " + b);
+    }
     await sendEmail(
-      "nft_delivery.js",
-      "👷👷‍♀️ WEB3DEV - NFT Recebido: " + nft_title,
+      'nft_delivery.js',
+      '👷👷‍♀️ WEB3DEV - NFT Recebido: ' + nft_title,
       user.email,
       params
-    );
-  });
+    )
+  })
 }
 
 module.exports = { mint }
