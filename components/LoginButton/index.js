d4c9f1b988fc625cbda9bb8de95ff85e3a5b9a65
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-06-17 22:41:23 -0300

    [124]: Refac > Extract login buttons to a reusable component and reuse it on signup page

---
+++ components/LoginButton/index.js
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function LoginButton(props) {
+  const { imgSrc, textContent, id, imgSize, loginGithub, loginGoogle } = props
+  return (
+    <>
+      {id.includes('github') ? (
+        <section className="flex items-center justify-center">
+          <button
+            id={id}
+            className={`dark:hover:bg-grey-400 fw-bold mb-5 flex
+            w-full cursor-pointer items-center justify-evenly rounded-lg border border-gray-700 bg-white-100  text-xl text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-1 dark:text-black-200`}
+            onClick={() => loginGithub()}
+          >
+            <Image src={imgSrc} alt={textContent} width={imgSize} height={imgSize} />
+            {textContent}
+          </button>
+        </section>
+      ) : (
+        <section className="flex w-full items-center justify-center">
+          <Image src={imgSrc} alt={textContent} width={imgSize} height={imgSize} />
+          <button
+            id={id}
+            className="cursor-pointer border-none bg-transparent hover:underline"
+            onClick={() => loginGoogle()}
+          >
+            {textContent}
+          </button>
+        </section>
+      )}
+    </>
+  )
+}
