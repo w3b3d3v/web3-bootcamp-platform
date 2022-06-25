431b980fece0913906f06f302b082d89020e92e2
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-06-24 23:17:52 -0300

    WIP: Extract functions from component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,89 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import useAuth from '../../hooks/useAuth';
+import React, { useState } from 'react'
+import useAuth from '../../hooks/useAuth'
 
 export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort }) {
-  const getCourse = course;
+  const getCourse = course
   const { user } = useAuth()
-  if(!getCourse?.sections) return null;
+  if (!getCourse?.sections) return null
   // list all the section as tabs
-  const [activeTab, setActiveTab] = useState(
-    Object.keys(getCourse?.sections).sort()
-  );
+  const [activeTab, setActiveTab] = useState(Object.keys(getCourse?.sections).sort())
   const toggle = (tab) => {
-    if(activeTab !== tab) setActiveTab(tab);
-  };
-  const checkLessons = () => {
-    const { list, courseSectionsLength } = getLessons()
-    const userLessons = lessonsSubmitted.filter(item => item.user == user.uid);
-    const userLessonsSubmittedInCurrentCohort = userLessons.filter(item => item.cohort === cohort?.id);
-    const sectionsCompleted = userLessonsSubmittedInCurrentCohort.map(lesson => {
-      return list.map(item => (item.section == lesson.section && item.file == lesson.lesson && item));
-    }).map(item => item.filter(Boolean)).flat();
-    const sectionsCompletedInCurrentCohort = sectionsCompleted.map(item => item.section).reduce(function(obj, b) {
-      obj[b] = ++obj[b] || 1;
-      return obj;
-    }, {});
-    const sections = Object.keys(courseSectionsLength);
-    const completed = sections.map(section => {
-      return {
-        section,
-        completed: sectionsCompletedInCurrentCohort[section] ? sectionsCompletedInCurrentCohort[section] : 0,
-        total: courseSectionsLength[section]
-      };
-    });
-    return completed;
-  };
-  const getLessons = () => {
-    const list = [];
-    const courseSectionsLength = {};
-    Object.keys(course?.sections).sort().map((section) => {
-      course?.sections[section].map((lesson) => {
-        courseSectionsLength[section] = courseSectionsLength[section] ? courseSectionsLength[section] + 1 : 1;
-        list.push({ section, ...lesson }
-        );
-      });
-    });
-    return { list, courseSectionsLength };
+    if (activeTab !== tab) setActiveTab(tab)
   }
-  const checkSectionCompleted = (section) => {
-    if(checkLessons() && section) {
-      return checkLessons().map(item => item?.section == section && item?.completed == item?.total).filter(Boolean)[0];
-    }
-  };
-  const checkCurrentSection = (section) => {
-    if(checkLessons()) {
-      if(checkLessons().find(item => item.completed < item.total)?.section == section) return 'bg-violet-600';
-    }
-  };
+  console.log(cohort)
   return (
-    <div className="flex flex-col rounded-lg bg-white-100 shadow-xl dark:bg-black-200 p-4">
+    <div className="flex flex-col rounded-lg bg-white-100 p-4 shadow-xl dark:bg-black-200">
       <div className="flex flex-row justify-between">
-        {Object.keys(getCourse?.sections).sort().map((section) => {
-          return (
-            <div className={'flex justify-between'} key={section}>
-              {isLessonPage ? (
-                <div
-                  className={`item flex-grow rounded px-4 py-2 font-bold text-black-100 shadow-lg hover:no-underline dark:text-white-100  ${
-                    checkSectionCompleted(section) ? 'bg-green-500 ' : checkCurrentSection(section)
-                  } `}
-                >
-                  <p className="m-0 p-0">{section?.replace('Section_', 'Seção ')}</p>
-                </div>
-              ) : (
-                <a
-                  href={`#${!isLessonPage ? section : ''}`}
-                  onClick={() => toggle(section)}
-                  className={`item flex-grow rounded px-4 py-2 font-bold text-black-100 shadow-lg hover:no-underline dark:text-white-100 ${
-                    checkSectionCompleted(section) ? 'bg-green-500 ' : checkCurrentSection(section)
-                  } `}
-                >
-                  <p className="m-0 p-0">{section?.replace('Section_', 'Seção ')}</p>
-                </a>
-              )}
-            </div>
-          )
-        })}
+        {Object.keys(getCourse?.sections)
+          .sort()
+          .map((section) => {
+            return (
+              <div className={'flex justify-between'} key={section}>
+                {isLessonPage ? (
+                  <div
+                    className={`item flex-grow rounded px-4 py-2 font-bold text-black-100 shadow-lg hover:no-underline dark:text-white-100  ${
+                      checkSectionCompleted(section, course, lessonsSubmitted, cohort, user?.id)
+                        ? 'bg-green-500 '
+                        : checkCurrentSection(section, course, lessonsSubmitted, cohort, user?.id)
+                    } `}
+                  >
+                    <p className="m-0 p-0">{section?.replace('Section_', 'Seção ')}</p>
+                  </div>
+                ) : (
+                  <a
+                    href={`#${!isLessonPage ? section : ''}`}
+                    onClick={() => toggle(section)}
+                    className={`item flex-grow rounded px-4 py-2 font-bold text-black-100 shadow-lg hover:no-underline dark:text-white-100 ${
+                      checkSectionCompleted(section, course, lessonsSubmitted, cohort, user?.id)
+                        ? 'bg-green-500 '
+                        : checkCurrentSection(section, course, lessonsSubmitted, cohort, user?.id)
+                    } `}
+                  >
+                    <p className="m-0 p-0">{section?.replace('Section_', 'Seção ')}</p>
+                  </a>
+                )}
+              </div>
+            )
+          })}
       </div>
     </div>
-  );
-};
+  )
+}
