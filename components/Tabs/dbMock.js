af310b023c2bac429811e8f4f08b5d8be110ec7c
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-06-25 11:42:06 -0300

    Fix: use more accurate data according to real db

--- components/Tabs/dbMock.js
+++ components/Tabs/dbMock.js
@@ -4,11 +4,36 @@ export const course = {
     Section_1: [
       { file: 'Lesson_1_Get_Your_Local_Ethereum_Network_Running.md' },
       {
-        file: 'Lesson_3_Connect_Wallet_To_Web_App.md',
+        file: 'Lesson_2_Write_Your_First_Smart_Contract.md',
+      },
+      {
+        file: 'Lesson_3_Compile_Contract_Locally.md',
+      },
+      {
+        file: 'Lesson_4_Store_Data_On_Smart_Contract.md',
+      },
+    ],
+    Section_2: [
+      { file: 'Lesson_1_Setup_React_App.md' },
+      {
+        file: 'Lesson_2_Deploy_To_Real_Testnet.md',
       },
     ],
   },
 }
+
+export const submittedFirstLesson = [
+  {
+    cohort: 'lloRUGwzJAG4Lj2B3bmJ',
+    cohort_id: 'lloRUGwzJAG4Lj2B3bmJ',
+    content: { type: 'text', value: 'um pequeno feedback' },
+    lesson: 'Lesson_1_Welcome.md',
+    section: 'Section_0',
+    user: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
+    user_id: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
+  },
+]
+
 export const lessonsSubmitted = [
   {
     cohort: 'lloRUGwzJAG4Lj2B3bmJ',
@@ -32,7 +57,9 @@ export const lessonsSubmitted = [
 
 export const section = Object.keys(course.sections)
   .sort()
-  .map((section) => section)[0]
+  .map((section) => section)
+
+export const sectionZero = 'Section_0'
 
 export const cohort = {
   courseId: 'Solidity_And_Smart_Contracts',
