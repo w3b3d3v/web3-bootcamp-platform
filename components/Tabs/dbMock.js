5650ca40fa9af7f285387250dfb20616143f3d42
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-06-25 00:24:30 -0300

    Fix: dbmock

--- components/Tabs/dbMock.js
+++ components/Tabs/dbMock.js
@@ -1,10 +1,10 @@
 export const course = {
   sections: {
-    Section_0: [{ file: 'Lesson_1_Welcome' }],
+    Section_0: [{ file: 'Lesson_1_Welcome.md' }],
     Section_1: [
-      { file: 'Lesson_1_What_are_we_building' },
+      { file: 'Lesson_1_Get_Your_Local_Ethereum_Network_Running.md' },
       {
-        file: 'Lesson_2_Creating_React_App',
+        file: 'Lesson_3_Connect_Wallet_To_Web_App.md',
       },
     ],
   },
@@ -14,8 +14,8 @@ export const lessonsSubmitted = [
     cohort: 'lloRUGwzJAG4Lj2B3bmJ',
     cohort_id: 'lloRUGwzJAG4Lj2B3bmJ',
     content: { type: 'text', value: 'um pequeno feedback' },
-    lesson: 'Lesson_4_Store_Data_On_Smart_Contract.md',
-    section: 'Section_1',
+    lesson: 'Lesson_1_Welcome.md',
+    section: 'Section_0',
     user: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
     user_id: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
   },
@@ -23,8 +23,8 @@ export const lessonsSubmitted = [
     cohort: 'lloRUGwzJAG4Lj2B3bmJ',
     cohort_id: 'lloRUGwzJAG4Lj2B3bmJ',
     content: { type: 'text', value: 'teste' },
-    lesson: 'Lesson_3_Connect_Wallet_To_Web_App.md',
-    section: 'Section_2',
+    lesson: 'Lesson_1_Get_Your_Local_Ethereum_Network_Running.md',
+    section: 'Section_1',
     user: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
     user_id: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
   },
@@ -32,13 +32,14 @@ export const lessonsSubmitted = [
 
 export const section = Object.keys(course.sections)
   .sort()
-  .map((section) => section)
+  .map((section) => section)[0]
 
 export const cohort = {
   courseId: 'Solidity_And_Smart_Contracts',
   endDate: new Date('2022, 06, 20'),
-  id: 'LNYQZCn5p9amT9VUJVhs',
+  id: 'lloRUGwzJAG4Lj2B3bmJ',
   kickoffEndTime: new Date('2022, 06, 19'),
   kickoffStartTime: new Date('2022, 06, 19'),
   startDate: new Date('2022, 06, 19'),
 }
+export const user = { uid: 'tFOYKWIm6OMlKHtmxarRfVY77W63' }
