21a41c8444e432f16b974ba4a74b7d9a6940cd9b
Paulo Bordignon <p.h_@outlook.com>
2022-07-01 23:28:16 -0300

    feat(all): new component to general profile data

---
+++ components/Textarea/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Textarea = React.forwardRef((props, ref) => {
+  return (
+    <>
+      <div class="flex flex-col">
+        <label for={props?.id} class="text-sm font-medium leading-none text-black-200 dark:text-gray-100 mb-2">
+          {props?.label}
+        </label>
+        <textarea
+          type={props?.type || 'text'}
+          ref={ref}
+          id={props?.id}
+          placeholder={props?.placeholder}
+          defaultValue={props?.defaultValue}
+          onChange={props?.onChange}
+          class="w-full rounded-lg border-2 p-2 border-solid text-black-300 dark:text-white-100 
+            font-medium text-sm font-sans focus:outline-primary-200 resize-y mb-3"
+        >
+        </textarea>
+      </div>
+    </>
+  )
+})
