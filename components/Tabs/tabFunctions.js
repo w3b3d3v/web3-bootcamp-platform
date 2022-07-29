cc0b23b90641d21af02b69af36331b70d64b2091
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-07-28 21:42:19 -0300

    [Refac]: Extract generic functions to be reusable

--- components/Tabs/tabFunctions.js
+++ components/Tabs/tabFunctions.js
@@ -1,71 +1,45 @@
-export const getLessons = (course) => {
-  const list = []
-  const lessonsBySection = {}
-  Object.keys(course?.sections)
-    .sort()
-    .map((section) => {
-      course?.sections[section].map((lesson) => {
-        lessonsBySection[section] = lessonsBySection[section] ? lessonsBySection[section] + 1 : 1
-        list.push({ section, ...lesson })
-      })
-    })
-  return { list, lessonsBySection }
+import {
+  completedResult,
+  filterUserLessons,
+  getLessonsByCourse,
+} from '../../lib/Models/withoutDB/lessons'
+import {
+  checkSectionsCompleted,
+  reduceSectionsCompleted,
+} from '../../lib/Models/withoutDB/sections'
+
+export const checkLessonsSubmitted = (course, lessonsSubmitted, user_id) => {
+  const { list, lessonsBySection } = getLessonsByCourse(course)
+
+  const userLessons = filterUserLessons(lessonsSubmitted, user_id)
+
+  const sectionsCompleted = checkSectionsCompleted(userLessons, list)
+
+  const completedSectionNumbers = reduceSectionsCompleted(sectionsCompleted)
+
+  return completedResult(lessonsBySection, completedSectionNumbers)
 }
 
-export const checkLessonsSubmitted = (course, lessonsSubmitted, cohort, user_id) => {
-  const { list, lessonsBySection } = getLessons(course)
-  const userLessons = filterUserLessons()
-
-  const sectionsCompleted = checkSectionsCompleted()
-
-  const completedSectionNumbers = reduceSectionsCompleted()
-
-  const completed = completedResult()
-
-  function filterUserLessons() {
-    return lessonsSubmitted.filter((item) => item.user === user_id)
-  }
-
-  function checkSectionsCompleted() {
-    return userLessons
-      .map((lesson) => {
-        return list.map(
-          (item) => item.section == lesson.section && item.file == lesson.lesson && item
-        )
-      })
-      .map((item) => item.filter(Boolean))
-      .flat()
-  }
-
-  function reduceSectionsCompleted() {
-    return sectionsCompleted
-      .map((item) => item.section)
-      .reduce(function (obj, currSection) {
-        obj[currSection] = ++obj[currSection] || 1
-        return obj
-      }, {})
-  }
-
-  function completedResult() {
-    return Object.keys(lessonsBySection).map((section) => {
-      return {
-        section,
-        completed: completedSectionNumbers[section] ? completedSectionNumbers[section] : 0,
-        total: lessonsBySection[section],
-      }
-    })
-  }
-
-  return completed
+export const checkSections = (course, lessonsSubmitted, section, user_id) => {
+  const completedLessonsBySection = checkLessonsSubmitted(course, lessonsSubmitted, user_id)
+  const isSectionCompleted = checkSectionIsCompleted(completedLessonsBySection, section)
+  const currentSection =
+    completedLessonsBySection.find((item) => item.completed < item.total)?.section == section
+  return { isSectionCompleted, currentSection }
 }
 
-export const checkSections = (course, lessonsSubmitted, cohort, section, user_id) => {
-  const lessons = checkLessonsSubmitted(course, lessonsSubmitted, cohort, user_id)
-  const completedSection = lessons
+export const checkSectionIsCompleted = (completedLessonsBySection, section) => {
+  return completedLessonsBySection
     .map((item) => item?.section == section && item?.completed == item?.total)
     .filter(Boolean)[0]
-  const currentSection = lessons.find((item) => item.completed < item.total)?.section == section
-  if (completedSection) return 'bg-green-500'
-  if (currentSection) return 'bg-violet-600'
-  return ''
+}
+
+export const colorTab = (isSectionCompleted, currentSection) => {
+  if (isSectionCompleted) {
+    return 'bg-green-500'
+  } else if (currentSection) {
+    return 'bg-violet-600'
+  } else {
+    return ''
+  }
 }
