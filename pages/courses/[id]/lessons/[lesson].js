7287fdcd2da0fcbadbf14a8fd6fa9bafe45af1ef
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-07-29 18:44:17 -0300

    [Rebase]: Fix conflicts through rebase

--- pages/courses/[id]/lessons/[lesson].js
+++ pages/courses/[id]/lessons/[lesson].js
@@ -38,6 +38,7 @@ function Lessons({ course, lesson }) {
   let testUrl
   useEffect(async () => {
     setCohorts(await getAllCohorts())
+    getSubmissionData()
   }, [])
 
   useEffect(async () => {
