ea13fa762697e41d99aa2acfd8c41311da24e02b
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-08-02 11:29:39 -0300

    [Change]: Solidity specific woman cover

--- components/Home/Inovadoras/Cover/index.js
+++ components/Home/Inovadoras/Cover/index.js
@@ -7,7 +7,7 @@ export default function Cover() {
       <Image
         width={400}
         height={400}
-        src="https://firebasestorage.googleapis.com/v0/b/web3dev-development.appspot.com/o/courses_cover%2FSolidity_And_Smart_Contracts.png?alt=media&token=b0e44fe0-02a5-4363-97de-fcf0a0752364"
+        src="https://firebasestorage.googleapis.com/v0/b/web3dev-development.appspot.com/o/courses_cover%2Fsolidity_female.png?alt=media&token=ddbb1afd-5042-43a9-ba59-266c2c40476a"
         className="rounded-3xl"
       />
     </div>
