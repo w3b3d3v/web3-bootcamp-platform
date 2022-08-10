848088737dbfe54a1e265af877e3d65510d878bd
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-08-09 23:52:04 -0300

    [Refac]: Remove fixture and create fetch function

---
+++ tests/lib/fetchDB.js
@@ -0,0 +1,22 @@
+import { getCourse } from '../../lib/course'
+import { getLessonsByCourse, getLessonsSubmittedBySection } from '../../lib/lessons'
+import { checkSectionsCompleted, lessonsCompletedBySection } from '../../lib/sections'
+
+let user_id = 'tFOYKWIm6OMlKHtmxarRfVY77W63'
+
+export async function fetchDB() {
+  const course = await getCourse('Solidity_And_Smart_Contracts')
+  const list = getLessonsByCourse(course).list
+  const lessonsBySection = getLessonsByCourse(course).lessonsBySection
+  const lessons = await getLessonsSubmittedBySection(user_id)
+  const sectionsCompleted = await checkSectionsCompleted(lessons, list)
+  const userLessonsCompletedBySection = lessonsCompletedBySection(sectionsCompleted)
+  return {
+    course,
+    lessons,
+    list,
+    lessonsBySection,
+    sectionsCompleted,
+    userLessonsCompletedBySection,
+  }
+}
