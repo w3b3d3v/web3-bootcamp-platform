26d5752e9d062c5dcefaa66494a2bd53cf20d541
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-08-09 23:54:39 -0300

    [Fix]: Color Tab functions test done separate from the others tests

--- components/Tabs/index.spec.js
+++ components/Tabs/index.spec.js
@@ -1,99 +1,53 @@
-import '@testing-library/jest-dom'
-import {
-  course,
-  sectionZero,
-  submittedFirstLesson,
-  lessonsSubmitted,
-  user,
-} from '../../tests/__mocks__/fixtures/tab_db_mock'
-
+//import '@testing-library/jest-dom'
 import { checkLessonsSubmitted, checkSections, colorTab } from './tabFunctions'
-import { completedResult, getLessonsByCourse } from '../../lib/Models/lessons'
-import { checkSectionsCompleted, lessonsCompletedBySection } from '../../lib/Models/sections'
+import { fetchDB } from '../../tests/lib/fetchDB'
 
-describe('Lessons model', () => {
-  const { list, lessonsBySection } = getLessonsByCourse(course)
-  const sectionsCompleted = checkSectionsCompleted(lessonsSubmitted, list)
-  const userLessonsCompletedBySection = lessonsCompletedBySection(sectionsCompleted)
-
-  it('should getLesons', () => {
-    const testObj = {
-      Section_0: 1,
-      Section_1: 4,
-      Section_2: 2,
-    }
-    expect(list).toHaveLength(7)
-    expect(lessonsBySection).toStrictEqual(testObj)
-  })
-
-  it('should return lessons completed by section', () => {
-    const lessonsCompletedBySectionMock = {
-      Section_0: 1,
-      Section_1: 1,
-    }
-    expect(userLessonsCompletedBySection).toStrictEqual(lessonsCompletedBySectionMock)
-  })
-
-  it('should return total lessons and completed lessons by section', () => {
-    const lessonsCompletedBySectionMock = [
-      {
-        section: 'Section_0',
-        total: 1,
-        completed: 1,
-      },
-      {
-        section: 'Section_1',
-        total: 4,
-        completed: 1,
-      },
-      {
-        section: 'Section_2',
-        total: 2,
-        completed: 0,
-      },
-    ]
-    const result = completedResult(lessonsBySection, userLessonsCompletedBySection)
-    expect(result).toStrictEqual(lessonsCompletedBySectionMock)
-  })
-})
+const db = {
+  course: null,
+  lessons: null,
+  user_id: 'tFOYKWIm6OMlKHtmxarRfVY77W63',
+}
 
 describe('Tabs util functions', () => {
+  beforeEach(async () => {
+    const { course, lessons } = await fetchDB()
+    db.course = course
+    db.lessons = lessons
+  })
+  
   it('should return green backgroundon section completed', () => {
+    const lesson = db.lessons.filter((lesson) => lesson.lesson === 'Lesson_1_Welcome.md')
     const { isSectionCompleted, currentSection } = checkSections(
-      course,
-      submittedFirstLesson,
-      sectionZero,
-      user.uid
+      db.course,
+      lesson,
+      'Section_0',
+      db.user_id
     )
     expect(colorTab(isSectionCompleted, currentSection)).toBe('bg-green-500')
   })
 
   it('should return purple background on uncompleted all lessons of a section', () => {
-    const sectionOne = 'Section_1'
-    const completed = checkSections(course, submittedFirstLesson, sectionZero, user.uid)
-    const notCompleted = checkSections(course, submittedFirstLesson, sectionOne, user.uid)
+    const completed = checkSections(db.course, db.lessons, 'Section_0', db.user_id)
+    const notCompleted = checkSections(db.course, db.lessons, 'Section_1', db.user_id)
     expect(colorTab(completed.isSectionCompleted, completed.currentSection)).toBe('bg-green-500')
     expect(colorTab(notCompleted.isSectionCompleted, notCompleted.currentSection)).toBe(
       'bg-violet-600'
     )
-    expect(checkSections(course, submittedFirstLesson, sectionZero, user.uid)).not.toBe(
-      'bg-violet-600'
-    )
+    expect(checkSections(db.course, db.lessons, 'Section_0', db.user_id)).not.toBe('bg-violet-600')
   })
 
   it('should return purple background on uncompleted all lessons of a section', () => {
-    const sectionTwo = 'Section_2'
     const { isSectionCompleted, currentSection } = checkSections(
-      course,
-      submittedFirstLesson,
-      sectionTwo,
-      user.uid
+      db.course,
+      db.lessons,
+      'Section_5',
+      db.user_id
     )
     expect(colorTab(isSectionCompleted, currentSection)).toBe('')
   })
 
   it('should return total lessons submitted', () => {
-    const lessonsSubmittedMock = [
+    const expectedResult = [
       {
         section: 'Section_0',
         completed: 1,
@@ -101,17 +55,25 @@ describe('Tabs util functions', () => {
       },
       {
         section: 'Section_1',
-        completed: 0,
-        total: 4,
+        completed: 4,
+        total: 5,
       },
       {
         section: 'Section_2',
-        completed: 0,
+        completed: 4,
+        total: 4,
+      },
+      {
+        section: 'Section_3',
+        completed: 2,
+        total: 2,
+      },
+      {
+        section: 'Section_4',
+        completed: 2,
         total: 2,
       },
     ]
-    expect(checkLessonsSubmitted(course, submittedFirstLesson, user.uid)).toStrictEqual(
-      lessonsSubmittedMock
-    )
+    expect(checkLessonsSubmitted(db.course, db.lessons, db.user_id)).toStrictEqual(expectedResult)
   })
 })
