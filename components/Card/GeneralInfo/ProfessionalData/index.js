5446a2e78f25bdecc5831300d14925a7e29d7ba2
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-08-24 11:03:28 -0300

    [Refac]: Create components for each step of user profile

---
+++ components/Card/GeneralInfo/ProfessionalData/index.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import Select from 'react-select'
+import { langOptions } from '../../../../lib/utils/constants'
+
+export default function ProfessionalData({ Controller, control, errors, register }) {
+  const colourStyles = {
+    control: (styles) => ({ ...styles, backgroundColor: 'rgb(59, 59, 59)' }),
+    option: (styles, { isFocused }) => {
+      return {
+        ...styles,
+        backgroundColor: isFocused ? '#8960F3' : 'rgb(59, 59, 59)',
+        color: 'white',
+      }
+    },
+    input: (styles) => ({ ...styles, color: 'white' }),
+  }
+
+  return (
+    <>
+      <div className="flex w-full flex-col gap-x-8 lg:flex-row">
+        <div className="grow sm:basis-2/12 ">
+          <div className="flex flex-col">
+            <label
+              htmlFor={'devExp'}
+              className="mb-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100"
+            >
+              Qual ano você começou a trabalhar com desenvolvimento?
+            </label>
+            <input
+              {...register('devExp')}
+              id={'devExp'}
+              placeholder="Insira o ano de início da sua experiência profissional com desenvolvimento"
+              className="mb-3 w-full rounded-lg border-2 border-solid p-2 font-sans 
+                          text-sm font-medium text-black-300 focus:outline-primary-200 dark:text-white-100"
+            />
+          </div>
+          <small className="text-red-500">{errors.devExp?.message}</small>
+        </div>
+        <div className="grow sm:basis-2/12">
+          <div className="flex flex-col">
+            <label
+              htmlFor={'blockchainExp'}
+              className="mb-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100"
+            >
+              Qual ano você começou a trabalhar com blockchain?
+            </label>
+            <input
+              {...register('blockchainExp')}
+              id={'blockchainExp'}
+              placeholder="Insira o ano de início da sua experiência profissional com blockchain"
+              className="mb-3 w-full rounded-lg border-2 border-solid p-2 font-sans 
+                          text-sm font-medium text-black-300 focus:outline-primary-200 dark:text-white-100"
+            />
+          </div>
+          <small className="text-red-500">{errors.blockchainExp?.message}</small>
+        </div>
+      </div>
+      <div className="grow sm:basis-5/12">
+        <div className="flex flex-col">
+          <label
+            htmlFor="technologies"
+            className="mb-1 text-sm font-medium leading-none text-black-200 dark:text-gray-100 lg:mb-9 xl:mb-6 2xl:mb-3"
+          >
+            Tecnologias com que trabalha
+          </label>
+          <Controller
+            id="technologies"
+            name="technologies"
+            control={control}
+            render={({ field }) => (
+              <Select
+                {...field}
+                instanceId="technologies"
+                isMulti
+                className="mb-3 w-full resize-y rounded-lg border-2 border-solid p-2 
+                            font-sans text-sm font-medium text-black-300 focus:outline-primary-200 dark:text-black-100"
+                options={langOptions.map((option) => ({
+                  label: option,
+                  value: option,
+                }))}
+                styles={colourStyles}
+              />
+            )}
+          />
+          <small className="text-red-500">{errors.linguagens?.message}</small>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ components/Card/GeneralInfo/SocialData/index.js
@@ -0,0 +1,122 @@
+import { GithubAuthProvider, linkWithCredential, linkWithPopup } from 'firebase/auth'
+import React from 'react'
+import { findSocialLinks } from '../../../../lib/user'
+import { Input } from '../../../Input'
+
+export default function SocialData({ Controller, control, errors, user }) {
+  const connectGithub = async (e) => {
+    e.preventDefault()
+    const provider = new GithubAuthProvider()
+    await linkWithCredential
+    await linkWithPopup(user, provider)
+      .then((result) => {
+        const github_id = result.user.providerData.find(
+          (item) => item.providerId == result.providerId
+        ).uid
+        fetch(`https://api.github.com/user/${github_id}`)
+          .then((res) => res.json())
+          .then(async (data) => {
+            await updateUserData(data.html_url)
+          })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+  return (
+    <>
+      <div className="grow sm:basis-6/12">
+        <div className="flex flex-col">
+          <Controller
+            name="twitter"
+            control={control}
+            defaultValue={findSocialLinks('twitter', user)?.url}
+            render={({ field }) => (
+              <Input
+                {...field}
+                label="Twitter"
+                defaultValue={findSocialLinks('twitter', user)?.url}
+                id="twitter"
+                placeholder="https://twitter.com/username"
+              />
+            )}
+          />
+          <small className="text-red-500">{errors.twitter?.message}</small>
+        </div>
+      </div>
+      <div className="grow sm:basis-6/12">
+        <Controller
+          name="linkedin"
+          control={control}
+          defaultValue={findSocialLinks('linkedin', user)?.url}
+          render={({ field }) => (
+            <Input
+              {...field}
+              label="Linkedin"
+              defaultValue={findSocialLinks('linkedin', user)?.url}
+              id="linkedin"
+              placeholder="https://linkedin.com/username"
+            />
+          )}
+        />
+        <small className="text-red-500">{errors.linkedin?.message}</small>
+      </div>
+      <div className="grow sm:basis-6/12">
+        {user?.socialLinks?.find((item) => item.name == 'github').url ? (
+          <>
+            <Controller
+              name="github"
+              control={control}
+              defaultValue={findSocialLinks('github', user)?.url}
+              render={({ field }) => (
+                <Input
+                  {...field}
+                  label="Github"
+                  defaultValue={findSocialLinks('github', user)?.url}
+                  id="github"
+                  placeholder="https://github.com/username"
+                />
+              )}
+            />
+            <small className="text-red-500">{errors.github?.message}</small>
+          </>
+        ) : (
+          <div className="flex flex-col">
+            <label
+              htmlFor="githubConnect"
+              className="mb-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100"
+            >
+              Github
+            </label>
+            <button
+              id="githubConnect"
+              className="text-white inline-flex cursor-pointer rounded-md border border-transparent bg-green-600 p-2
+                        text-base font-medium shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500
+                        focus:ring-offset-2 sm:w-auto sm:text-sm"
+              onClick={connectGithub}
+            >
+              Conectar Github
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="grow sm:basis-6/12 ">
+        <Controller
+          name="personalWebsite"
+          control={control}
+          defaultValue={findSocialLinks('personalWebsite', user)?.url}
+          render={({ field }) => (
+            <Input
+              {...field}
+              label="Site Pessoal"
+              defaultValue={findSocialLinks('personalWebsite', user)?.url}
+              id="personalWebsite"
+              placeholder="https://meuwebsite.com"
+            />
+          )}
+        />
+        <small className="text-red-500">{errors.personalWebsite?.message}</small>
+      </div>
+    </>
+  )
+}
