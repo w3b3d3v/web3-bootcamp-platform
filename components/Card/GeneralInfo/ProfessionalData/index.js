875481ac70918131f4e4be60089069951e228d1d
Rômulo Rodrigues <biorrodrigues@gmail.com>
2022-08-25 10:21:12 -0300

    [Feat]: Create new builder options for profile

--- components/Card/GeneralInfo/ProfessionalData/index.js
+++ components/Card/GeneralInfo/ProfessionalData/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Select from 'react-select'
-import { langOptions } from '../../../../lib/utils/constants'
+import { builderEmojis, builderOptions, langOptions } from '../../../../lib/utils/constants'
 
-export default function ProfessionalData({ Controller, control, errors, register }) {
+export default function ProfessionalData({
+  Controller,
+  control,
+  errors,
+  register,
+  getValues,
+  setValue,
+}) {
   const colourStyles = {
-    control: (styles) => ({ ...styles, backgroundColor: 'rgb(59, 59, 59)', border: '2px solid rgb(99 102 241)' }),
+    control: (styles) => ({
+      ...styles,
+      backgroundColor: 'rgb(59, 59, 59)',
+      border: '2px solid rgb(99 102 241)',
+    }),
     option: (styles, { isFocused }) => {
       return {
         ...styles,
@@ -13,6 +24,25 @@ export default function ProfessionalData({ Controller, control, errors, register
       }
     },
     input: (styles) => ({ ...styles, color: 'white' }),
+  }
+
+  const getEmojiByIndex = (index) => builderEmojis[index]
+
+  const updateUserData = (option) => {
+    if (getValues('builder')?.includes(option)) {
+      return setValue(
+        'builder',
+        getValues('builder').filter((builder) => builder !== option)
+      )
+    }
+    if (
+      !getValues('builder') ||
+      (!getValues('builder')?.includes(option) && getValues('builder').length < 3)
+    ) {
+      getValues('builder')
+        ? setValue('builder', [...getValues('builder'), option])
+        : setValue('builder', [option])
+    }
   }
 
   return (
@@ -57,7 +87,7 @@ export default function ProfessionalData({ Controller, control, errors, register
         <div className="flex flex-col">
           <label
             htmlFor="technologies"
-            className="mb-1 text-sm font-medium leading-none text-black-200 dark:text-gray-100 lg:mb-9 xl:mb-6 2xl:mb-3"
+            className="mb-1 py-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100 lg:mb-9 xl:mb-6 2xl:mb-3"
           >
             Tecnologias com que trabalha
           </label>
@@ -80,6 +110,29 @@ export default function ProfessionalData({ Controller, control, errors, register
             )}
           />
           <small className="text-red-500">{errors.linguagens?.message}</small>
+        </div>
+      </div>
+      <div className="py-2">
+        Que tipo de{' '}
+        <em>
+          <b>builder</b>
+        </em>{' '}
+        é você ? Selecione até 3 opções
+        <br />
+        <div className="flex flex-wrap items-center justify-center">
+          {builderOptions.map((option, index) => (
+            <div
+              {...register('builder')}
+              key={option}
+              onClick={() => updateUserData(option)}
+              className={`m-2 flex w-fit w-44 cursor-pointer flex-col items-center justify-center rounded-lg border-[0.5px] border-solid border-indigo-400 text-center text-white-200 hover:bg-indigo-400 ${
+                getValues('builder')?.includes(option) ? 'bg-indigo-400' : 'bg-transparent'
+              }`}
+            >
+              <p className="m-0">{getEmojiByIndex(index)}</p>
+              <p className="m-0">{option}</p>
+            </div>
+          ))}
         </div>
       </div>
     </>
