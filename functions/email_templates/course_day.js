4a4e2cf5dec4739cb66cd975bf855fbfd5a83594
Daniel Cukier <danicuki@gmail.com>
2022-09-15 11:39:05 -0300

    improve course day message

--- functions/email_templates/course_day.js
+++ functions/email_templates/course_day.js
@@ -14,7 +14,7 @@ function template(params) {
   }">Esse é o link do bootcamp</a>. 
         A live começa exatamente às 19h horário de Brasília no <a href="https://www.twitch.tv/web3dev">nosso Twitch</a>.
     <div><br></div>
-    <div><b>Esse projeto é praticamente todo assíncrono.</b>&nbsp;O único momento ao vivo será nossa "live". Se não puder participar, não se preocipe. Ela também será gravada e disponibilizada para você assistir!</div>
+    <div><b>Esse projeto é praticamente todo assíncrono.</b>&nbsp;O único momento ao vivo será nossa "live". Se não puder participar, não se preocupe. Ela também será gravada e disponibilizada para você assistir!</div>
     <div><br></div>
     <div><b>O material do bootcamp será aberto oficialmente as 19h, junto com a live <a href="https://bootcamp.web3dev.com.br/courses/${
       params.cohort.course_id
@@ -24,11 +24,11 @@ function template(params) {
     <h3>Algumas informações importantes:</h3>
     <div>1. Se você terminar o projeto até ${d(
       params.cohort.endDate
-    )}, você terá direito a receber um <b> NFT exclusivo </b>. Você também será adicionado ao cargo 🎓 graduad@ no nosso Discord e terá acesso a vagas de trabalho web3 em nossos parceiros.</div>
+    )}, você terá direito a receber um <b> NFT exclusivo </b>. Você também receberá o cargo 🎓 graduad@ no nosso Discord e terá acesso a vagas de trabalho web3 em nossos parceiros.</div>
     <div><br></div>
     <div>2. Esse é o único e-mail que eu vou mandar. O resto estará acontecendo no canal secreto "#⛺ | ${
       params.cohort.discord_channel
-    }" que você terá acesso caso esteja inscrito no bootcamp. Para isso, você precisa "linkar" seu Discord na plataforma de bootcamp antes de começar o projeto!
+    }" que você terá acesso caso se esteja bootcamp. Para isso, você precisa "linkar" seu Discord na plataforma de bootcamp antes de começar o projeto!
     </div>
     <div><br></div>
     <div>3. Por favor, não chama isso de "curso"! É um projeto, bootcamp, hackday, chama do que quiser. Só quero que você faça um trabalho legal com um pouco de guia :-).</div>
--- scripts/send_cohort_email.sh
+++ scripts/send_cohort_email.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-COHORT_ID=iWzxss5FWNxlBf2I4UNx
+COHORT_ID=5HLP7Ttl6hmA5I5VRYHC
 TEMPLATE=course_day
-SUBJECT="🎮Crie seu própio NFT Game 👾"
+# SUBJECT="🎮 Crie seu própio NFT Game 👾"
+SUBJECT="🤓 Crie seu primeiro Smart Contract com Solidity"
 wget "http://localhost:5001/web3dev-bootcamp/us-central1/sendEmailToAllUsers?cohort_id=$COHORT_ID&template=$TEMPLATE&subject=$SUBJECT"
