5d6c0bf1d9636121d7e89e823a5cafa2dd054dcb
Daniel Cukier <danicuki@gmail.com>
2022-09-26 15:45:56 -0300

    changed home to solana course

--- components/Home/index.js
+++ components/Home/index.js
@@ -9,20 +9,23 @@ export default function Main() {
       <div className="flex flex-col items-center justify-center text-start md:w-1/2">
         <div>
           <div className="text-5xl font-bold">
-            Crie seu própio 
+            Crie seu primeiro
             <span className="lp-gradient-text bg-gradient-to-r from-green-400 to-teal-400"></span>{' '}
             <span className="lp-gradient-text bg-gradient-to-r from-teal-400 to-fuchsia-500">
-            mini-jogo NFT{' '}
+              dApp Solana{' '}
             </span>
-            de turnos
+            com Rust
           </div>
           <p className="mt-8">
-          Um projeto para desenvolvedores curiosos que desejam aprender mais sobre a união entre criptomoedas e jogos fazendo o seu próprio jogo web3. Você irá mintar seus próprios NFTs, fazer personagens jogáveis com os NFTs que você mintou e permitir que usuários ganhem suas próprias criptomoedas conforme eles jogam o seu jogo!
+            Este é um projeto de muito aprendizado e diversão, de uma semana para desenvolvedores
+            curiosos que querem hackear Solana. Você aprenderá um pouco de Rust, escreverá e
+            implementará um programa Solana e conectará tudo a um aplicativo React web3 com o qual
+            qualquer pessoa com uma carteira Solana poderá interagir!
           </p>
         </div>
         <br />
         <div className="flex w-full justify-start">
-          <a href="/courses/NFT_Game">
+          <a href="/courses/Solana_And_Web3">
             <Button id="wish-to-sign-in" ref={ref}>
               Quero me inscrever!
             </Button>
