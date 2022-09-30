3c62f3b938bb45a4e83b37742ad907ce26e194f6
Vitor <vitor.mancio@ggmail.com>
2022-09-30 11:39:39 -0300

    update image url

--- components/Home/Inovadoras-nft-collection/Cover/index.js
+++ components/Home/Inovadoras-nft-collection/Cover/index.js
@@ -7,7 +7,7 @@ export default function Cover() {
       <Image
         width={400}
         height={400}
-        src="https://firebasestorage.googleapis.com/v0/b/web3dev-development.appspot.com/o/courses_cover%2Fsolidity_female.png?alt=media&token=ddbb1afd-5042-43a9-ba59-266c2c40476a"
+        src="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FNFT_COLLECTION_INOVADORAS.png?alt=media&token=f6fae749-7356-45ab-837f-7a0da08fb5a3"
         className="rounded-3xl"
       />
     </div>
