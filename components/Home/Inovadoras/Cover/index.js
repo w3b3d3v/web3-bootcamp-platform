8776bc9125a01f44ac8f3b34c35c0e216f4e09e3
Vitor <vitor.mancio@ggmail.com>
2022-10-02 08:33:57 -0300

    update meta url image solidity for girls team

--- components/Home/Inovadoras/Cover/index.js
+++ components/Home/Inovadoras/Cover/index.js
@@ -7,7 +7,7 @@ export default function Cover() {
       <Image
         width={400}
         height={400}
-        src="https://firebasestorage.googleapis.com/v0/b/web3dev-development.appspot.com/o/courses_cover%2Fsolidity_female.png?alt=media&token=ddbb1afd-5042-43a9-ba59-266c2c40476a"
+        src="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2Fsolidity_female.png?alt=media&token=355e7922-8ccd-4fb7-8975-a23348f6f4f7"
         className="rounded-3xl"
       />
     </div>
