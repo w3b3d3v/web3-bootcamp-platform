34355571deddd521929b60ad3c31b92fa79cc8c8
Vitor <vitor.mancio@ggmail.com>
2022-10-04 16:21:36 -0300

    [refactor] add max-widht  in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@ module.exports = {
   content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
   darkMode: 'class', // or 'media'
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+
+      'maxsm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {
       colors: {
         primary: {
