c7a41cf8f99198cc100dafe7c1c3480828f75818
Vitor <vitor.mancio@ggmail.com>
2022-10-04 16:47:52 -0300

    [fix] ajust to show courses without login(mobile)

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -470,7 +470,6 @@ export default function Navbar() {
                         </a>
                       ))}
 
-                      {user ? (
                         <a
                           id="mobile-menu-side-bar-courses"
                           href="/courses"
@@ -484,7 +483,7 @@ export default function Navbar() {
                             </div>
                           </li>
                         </a>
-                      ) : null}
+                      
                     </ul>
                   </div>
                   <div className="w-full pt-4">
