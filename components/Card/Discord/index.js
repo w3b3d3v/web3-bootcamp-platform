4a7e6fe7a6c0bfbf3ed11a2f2261f66edae88e36
Vitor <vitor.mancio@ggmail.com>
2022-10-18 03:07:50 -0300

    add logo images, replace and discord connect content

--- components/Card/Discord/index.js
+++ components/Card/Discord/index.js
@@ -4,6 +4,7 @@ import { Button } from '../../Button'
 import { auth } from '../../../firebase/initFirebase'
 import { getUserFromFirestore, updateUserDiscordIdinFirestore } from '../../../lib/user'
 import { onAuthStateChanged } from 'firebase/auth'
+import Image from 'next/image'
 
 export default function DiscordCard() {
   const { data: session } = useSession()
@@ -40,17 +41,21 @@ export default function DiscordCard() {
         <div className="rounded-lg bg-white-100 shadow-xl dark:bg-black-200">
           <div className="flex">
             <div className="px-6 py-5">
-              <p className="text-base font-medium leading-none text-black-200 dark:text-gray-100">
-                ❌ Conecte seu Discord
+              
+                <p className="text-base font-medium text-black-200 dark:text-gray-100">
+                  ❌ Você ainda não liberou a categoria exclusiva do bootcamp no <span className='text-blue-400 font-semibold'>discord</span>
+                </p>
+                
+              
+              <p className="pt-2 text-sm leading-5 text-gray-500 dark:text-gray-400">
+                <p className='dark:text-white-100 text-black-400'><strong><span className='text-orange-600 '>Aviso: </span>Você precisa estar conectado em nosso discord antes clicar em Liberar Salas, caso não esteja conectado  clique <a href='https://discord.com/invite/UXDUzG2hhE' alt='Link para conectar discord' className=''>  aqui</a>.</strong></p>
+                Quando você tiver acesso,não deixe de interagir!
               </p>
-              <p className="pt-2 text-xs leading-5 text-gray-500 dark:text-gray-400">
-                Tenha acesso aos canais supersecretos para em nosso grupo. Quando você tiver acesso,
-                não deixe de dizer olá!
-              </p>
-              <div className="pt-4">
+              <div className="pt-4 flex gap-2 items-center">
                 <Button ref={ref} id="connect-discord" onClick={() => signIn('discord')}>
-                  Conectar Discord
+                  Liberar Salas
                 </Button>
+                <Image src="/assets/img/discord_icon.svg" width="43" height="43"/>
               </div>
             </div>
           </div>
@@ -65,9 +70,13 @@ export default function DiscordCard() {
                   ✅ {user?.discord?.username || session?.discord.username} Conectado
                 </p>
                 <p className="pt-2 text-xs leading-5 text-gray-500 dark:text-gray-400">
-                  Estamos ansiosos para te conhecer, venha dar uma olhada!
+                  Explore as oportunidades na nossa comunidade,obrigado por ser um de nossos membros ❤️. 
                 </p>
-                <Button onClick={() => signIn('discord')}>Reconectar Discord</Button>
+                <p className='dark:text-white-100 text-black-400 text-sm'>Não se esqueca que cada bootcamp que você está inscrito tem uma categoria para você interagir.</p>
+              <div className="pt-4 flex gap-2 items-center">
+                <Button onClick={() => signIn('discord')}>Reconectar</Button>
+                <Image src="/assets/img/discord_icon.svg" width="43" height="43"/>
+              </div>
                 {/*<div className="pt-4">
                 <a className='cursor-pointer' onClick={() => disconnectDiscord()}>Desconectar</a>
                 </div>*/}
