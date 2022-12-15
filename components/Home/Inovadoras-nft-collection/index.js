db5e500d2148e489a15ea93df76bdffdb34724d2
vitordev <vitor.mancio@gmail.com>
2022-12-15 09:09:03 -0300

    [style]improve and bring all presentation page courses to the same html/css pattern

--- components/Home/Inovadoras-nft-collection/index.js
+++ components/Home/Inovadoras-nft-collection/index.js
@@ -5,29 +5,28 @@ import Cover from '../Inovadoras-nft-collection/Cover'
 function Main() {
   const ref = React.createRef()
   return (
-    <main className="max-w-7xl mx-auto flex flex-col justify-between px-8 mt-4 lg:mt-16 md:flex-row mb-14">
-      <div className="flex flex-col items-center justify-center text-start md:w-1/2">
+    <main className="max-w-7xl mx-auto mb-14 px-8 mt-4 flex flex-col justify-between lg:mt-16 lg:flex-row items-center gap-0 lg:gap-52">
+      <div className="flex flex-col items-center mx-auto flex-1">
         <div>
-          <div className="text-5xl font-bold">
+          <h1 className='text-4xl font-bold text-center sm:text-start m-0'>
             Construa sua coleção de  
             <span className="lp-gradient-text bg-gradient-to-r from-green-400 to-teal-400"></span>{' '}
             <span className="lp-gradient-text bg-gradient-to-r from-teal-400 to-fuchsia-500">
               NFTS{' '}
             </span>
-          </div>
+          </h1>
           <p className="mt-8">
             Um projeto onde você irá aprender a programar e gerar sua própria coleção de <b>NFT</b>, construa seu próprio React dApp para permitir que amigos conectem suas carteiras e interajam com seus NFTs. Perfeito para mulheres inovadoras, entusiastas em blockchain e desenvolvedoras.
           </p>
         </div>
         <br />
-        <div className="lg:self-start">
+        <div className="lg:self-start mb-8">
           <a href="/courses/NFT_Collection">
             <Button id="wish-to-sign-in" ref={ref}>
               Quero me inscrever!
             </Button>
           </a>
         </div>
-        <br />
       </div>
       <Cover />
     </main>
--- components/Home/index.js
+++ components/Home/index.js
@@ -5,11 +5,11 @@ import Cover from './Cover'
 export default function Main() {
   const ref = React.createRef()
   return (
-    <main className="max-w-7xl mx-auto mb-14 px-8 mt-4 flex flex-col justify-between lg:mt-16 lg:flex-row ">
+    <main className="max-w-7xl mx-auto mb-14 px-8 mt-4 flex flex-col justify-between lg:mt-16 lg:flex-row items-center gap-0 lg:gap-52">
       <div className="flex flex-col items-center mx-auto flex-1">
         <div>
         
-            <h1 className='text-4xl font-bold text-center sm:text-start'>
+            <h1 className='text-4xl font-bold text-center sm:text-start m-0'>
               Crie uma loja online pela rede blockchain
               <span className="lp-gradient-text bg-gradient-to-r from-green-400 to-teal-400"></span>{' '}
               <span className="lp-gradient-text bg-gradient-to-r from-teal-400 to-fuchsia-500">
@@ -35,14 +35,13 @@ export default function Main() {
           </p>
         </div>
         <br />
-        <div className="lg:self-start">
+        <div className="lg:self-start mb-8">
           <a href="/courses/Solana_Pay_Store">
             <Button id="wish-to-sign-in" ref={ref}>
               Quero me inscrever!
             </Button>
           </a>
         </div>
-        <br />
       </div>
       <div className='lg:m-0 mx-auto'>
       <Cover />
