7fda2030786370084b8418c15bc70c7542d16b22
vitordev <vitor.mancio@gmail.com>
2023-02-02 16:35:23 -0300

    [style] re-design profile page

--- components/Card/GeneralInfo/ProfessionalData/index.js
+++ components/Card/GeneralInfo/ProfessionalData/index.js
@@ -1,3 +1,4 @@
+import { Card, Input, Grid } from '@nextui-org/react'
 import React from 'react'
 import Select from 'react-select'
 import { builderEmojis, builderOptions, langOptions } from '../../../../lib/utils/constants'
@@ -47,48 +48,38 @@ export default function ProfessionalData({
 
   return (
     <>
-      <div className="flex w-full flex-col gap-x-8 lg:flex-row">
-        <div className="grow sm:basis-2/12 ">
-          <div className="flex flex-col">
-            <label
-              htmlFor={'devExp'}
-              className="mb-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100"
-            >
-              Qual ano você começou a trabalhar com desenvolvimento?
-            </label>
-            <input
-              {...register('devExp')}
-              id={'devExp'}
-              placeholder="Insira o ano de início da sua experiência profissional com desenvolvimento"
-              className="border-3 mb-3 w-full border-indigo-200 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 bg-transparent p-2 font-sans text-sm font-medium text-black-300 focus:outline-primary-200 dark:text-white-100"
-            />
-          </div>
-          <small className="text-red-500">{errors.devExp?.message}</small>
-        </div>
-        <div className="grow sm:basis-2/12">
+      <div className="text-center flex gap-2 flex-col">
+        <label
+          htmlFor={'devExp'}
+          className="mb-2 block text-sm font-medium leading-none text-black-200 dark:text-gray-100"
+        ></label>
+        <Input
+          {...register('devExp')}
+          labelPlaceholder={'Qual ano você começou a trabalhar com desenvolvimento?'}
+          width={'100%'}
+          id={'devExp'}
+          placeholder="Insira o ano de início da sua experiência profissional com desenvolvimento"
+        />
+        <small className="text-red-500">{errors.devExp?.message}</small>
+        <div className="mt-4">
           <div className="flex flex-col">
             <label
               htmlFor={'blockchainExp'}
-              className="mb-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100"
             >
-              Qual ano você começou a trabalhar com blockchain?
             </label>
-            <input
+            <Input
               {...register('blockchainExp')}
               id={'blockchainExp'}
-              placeholder="Insira o ano de início da sua experiência profissional com blockchain"
-              className="border-3 mb-3 w-full border-indigo-200 border-t-transparent border-r-transparent border-l-transparent border-b-indigo-500 bg-transparent p-2 font-sans text-sm font-medium text-black-300 focus:outline-primary-200 dark:text-white-100"
+              labelPlaceholder={"Insira o ano de início da sua experiência profissional com blockchain"}
             />
           </div>
           <small className="text-red-500">{errors.blockchainExp?.message}</small>
         </div>
       </div>
+
       <div className="grow sm:basis-5/12">
         <div className="flex flex-col">
-          <label
-            htmlFor="technologies"
-            className="mb-1 py-2 text-sm font-medium leading-none text-black-200 dark:text-gray-100 lg:mb-9 xl:mb-6 2xl:mb-3"
-          >
+          <label htmlFor="technologies" className="mb-3 mt-3">
             Tecnologias com que trabalha
           </label>
           <Controller
@@ -112,28 +103,32 @@ export default function ProfessionalData({
           <small className="text-red-500">{errors.linguagens?.message}</small>
         </div>
       </div>
+
       <div className="py-2">
-        Que tipo de{' '}
-        <em>
-          <b>builder</b>
-        </em>{' '}
+        Que tipo de <bold>builder</bold> {''}
         é você ? Selecione até 3 opções
         <br />
-        <div className="flex flex-wrap items-center justify-center">
-          {builderOptions.map((option, index) => (
-            <div
-              {...register('builder')}
-              key={option}
-              onClick={() => updateUserData(option)}
-              className={`m-2 flex w-fit w-44 cursor-pointer flex-col items-center justify-center rounded-lg border-[0.5px] border-solid border-indigo-400 text-center text-white-200 hover:bg-indigo-400 ${
-                getValues('builder')?.includes(option) ? 'bg-indigo-400' : 'bg-transparent'
-              }`}
-            >
-              <p className="m-0">{getEmojiByIndex(index)}</p>
-              <p className="m-0">{option}</p>
-            </div>
-          ))}
-        </div>
+        <Grid.Container gap={2} justify="center">
+          <div className="flex flex-wrap items-center justify-center">
+            {builderOptions.map((option, index) => (
+              <Grid xs={2}>
+                <Card isHoverable variant="bordered">
+                  <div
+                    {...register('builder')}
+                    key={option}
+                    onClick={() => updateUserData(option)}
+                    className={`m-2 flex cursor-pointer flex-col items-center justify-center rounded-lg text-center hover:bg-indigo-400 ${
+                      getValues('builder')?.includes(option) ? 'bg-indigo-400' : 'bg-transparent'
+                    }`}
+                  >
+                    <p className="m-0">{getEmojiByIndex(index)}</p>
+                    <p className="m-0">{option}</p>
+                  </div>
+                </Card>
+              </Grid>
+            ))}
+          </div>
+        </Grid.Container>
       </div>
     </>
   )
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
 import { withProtected } from '../../hooks/route'
 import GeneralInfoCard from '../../components/Card/GeneralInfo'
-import DiscordCard from '../../components/Card/Discord'
-import WalletCard from '../../components/Card/Wallet'
 
 function Profile() {
   return (
@@ -10,21 +8,7 @@ function Profile() {
       <Head>
         <title>Perfil -Web3Dev</title>
       </Head>
-      <main className="max-w-7xl mx-auto px-6 py-2 md:px-6 ">
-        <div className="flex flex-col gap-6 pb-6 lg:flex-row">
-          <div className="item flex-grow xl:max-w-7xl">
             <GeneralInfoCard />
-          </div>
-        </div>
-        <div className="flex flex-col lg:flex-row gap-11 content-end">
-          <div className="flex-1">
-            <DiscordCard />
-          </div>
-          <div className="flex-1">
-            <WalletCard />
-          </div>
-        </div>
-      </main>
     </>
   )
 }
