2990a09a3e02db3da11eb48e8c8b82e53ac6f0ed
vitordev <vitor.mancio@gmail.com>
2023-02-15 10:33:48 -0300

    [style] ajusts

--- styles/components/Card/SocialData/index.js
+++ styles/components/Card/SocialData/index.js
@@ -1,9 +1,9 @@
 import { styled } from '@nextui-org/react'
 
 export const Content = styled('div', {
-        height: '100vh',
         alignItems: 'center',
         justifyContent: 'center',
         maxW: '800px',
-        margin:'auto'
+        margin:'auto' 
 })
+
