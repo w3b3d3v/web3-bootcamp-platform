869ac38fef4a3588c3816564b5e767ab8f3aab7f
vitordev <vitor.mancio@gmail.com>
2023-02-16 09:33:38 -0300

    [refactor]remove not used multipage select

--- components/Card/GeneralInfo/ProfileFooter/index.js
+++ components/Card/GeneralInfo/ProfileFooter/index.js
@@ -32,27 +32,6 @@ export default function ProfileFooter({
   }
   return (
     <div data-testid="profile-footer">
-      {/* <div className='flex justify-between gap-56 mt-4 mb-4'>
-          <Button
-            onClick={previousFormStep}
-            customClass="rounded-lg w-24 h-10"
-            disabled={showPersonalData}
-            dataTestId="previous-profile-form-step"
-          >
-            Voltar
-          </Button>
-          
-          <Button
-            onClick={nextFormStep}
-            customClass="rounded-lg mb-2 w-24"
-            disabled={showProfessionalData}
-            dataTestId="next-profile-form-step"
-          >
-            Avançar
-          </Button>
-      
-      </div> */}
-
       <div className="mt-8 flex w-full justify-center">
         <Button
           type="submit"
