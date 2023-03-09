6cdac0cb22ffd28fb5d9370419d20262625ae282
vitordev <vitor.mancio@gmail.com>
2023-03-09 11:23:32 -0300

    [fix] remove home card gradient

--- components/Home/Cards/index.js
+++ components/Home/Cards/index.js
@@ -20,20 +20,18 @@ export default function HomeCards({ cards }) {
 
   return ( 
     <footer>
-      <div className="mx-auto max-w-4xl flex flex-col items-center justify-between py-4 text-center text-sm lg:flex-row lg:items-stretch font-bold ">
-        {cards.map((card, index) => (
-          <div
-            key={card}
-            className={`h-32 w-44 rounded-lg bg-gradient-to-br mb-6 mr-0 lg:mr-6 ${backgroundColor(
-              index
-            )} p-1 `}
-          >
-            <div className="text-black flex h-full w-full items-center rounded-l text-center shadow-xl  sm:p-1 md:p-3 lg:p-4 ">
-              {card}
+        <div className="mx-auto max-w-4xl flex flex-col items-center justify-between py-4 text-center text-sm lg:flex-row lg:items-stretch font-bold">
+          {cards.map((card, index) => (
+            <div
+              key={card}
+              className={`h-32 w-44 rounded-lg bg-gradient-to-br mb-6 mr-0 lg:mr-6 p-1 border`}
+            >
+              <div className="text-black flex h-full w-full items-center rounded-l text-center shadow-xl  sm:p-1 md:p-3 lg:p-4 ">
+                {card}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
       <br />
     </footer>
   )
