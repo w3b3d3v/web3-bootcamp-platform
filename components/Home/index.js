9b51cfb7a0bcfc9826f168c6c4a0a8fcf5cb8103
vitordev <vitor.mancio@gmail.com>
2023-03-09 17:49:42 -0300

    [fix/feat]Change home to Solana(next build projext) and improve ui design layout

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,43 +1,45 @@
 import React from 'react'
-import { Button } from '../Button'
+import { Text, Button } from '@nextui-org/react'
 import Cover from './Cover'
 
 export default function Main() {
   const ref = React.createRef()
   return (
-    <main className="max-w-7xl mx-auto mb-14 px-8 mt-4 flex flex-col justify-between lg:mt-16 lg:flex-row items-center gap-0 lg:gap-52">
+    <div className="max-w-7xl mx-auto mb-14 px-8 mt-4 flex flex-col justify-between lg:mt-16 lg:flex-row items-center gap-0 lg:gap-52">
       <div className="flex flex-col items-center mx-auto flex-1">
         <div>
-           <h1 className="m-0 text-center text-4xl font-bold sm:text-start">
-            Contrua um Dapp de{' '}
-            <span className="lp-gradient-text bg-gradient-to-r from-green-400 to-teal-400"></span>{' '}
-            <span className="lp-gradient-text bg-gradient-to-r from-teal-400 to-fuchsia-500 ">
-              DAO{' '}
-            </span>
-             com sistema de votações usando Javascript
-          </h1>
-          <p>
-           Este projeto é uma maneira rápida e prática pra você aprender como criar um Dapp de uma DAO com um Front End em React.js, e o mais legal é que está 100% ensinado em português.
-          </p>
-          <p>Tudo isso usando apenas Javascript, fazendo deploy no final com ajuda de um passo a passo bem simples.</p>
-          <p>Não fique de fora deste novo modelo descentralizado das novas organizações WEB3.</p>
-          <p>
-            <strong>O mais íncrivel :</strong> Seus amigos e colegas podem testar seu app após o deploy ser feito 😊
-            Além de poder crescer seu portfólio em WEB3.
-          </p>
+          <div className='max-w-lg m-auto' >
+            <Text h1 auto className="text-center font-bold mb-4">
+              Contrua um Dapp de{' '}
+              <span className="lp-gradient-text bg-gradient-to-r from-green-400 to-teal-400"></span>{' '}
+              <span className="lp-gradient-text bg-gradient-to-r from-teal-400 to-fuchsia-500 ">
+                DAO{' '}
+              </span>
+              com sistema de votações usando Javascript
+            </Text>
+          </div>
+          <div className='flex justify-center mb-7 text-justify'>
+            <Cover />
+          </div>
+          <div className="max-w-2xl">
+            <Text>
+              Este é um projeto prático e intensivo de <strong>10 dias</strong> que oferece uma oportunidade única para desenvolvedores aprofundarem seus conhecimentos em <strong>Solana, Rust e React</strong>. Você aprenderá a criar um aplicativo na blockchain Solana usando Rust para desenvolver contratos inteligentes e React para construir a interface de usuário. Ao concluir o Build, você terá todas as habilidades necessárias para criar um aplicativo em Solana permitindo que usuários com uma carteira Solana interajam facilmente com sua plataforma.
+            </Text>
+            <Text>
+              Você também terá a chance de se conectar com outros desenvolvedores em nossa comunidade exclusiva e aprimorar ainda mais suas habilidades em tecnologias emergentes. Junte-se a nós agora e comece a construir o futuro da web3!
+            </Text>
+          </div>
         </div>
         <br />
-        <div className="lg:self-start mb-8">
+        <div className="lg:self-start m-auto mb-8">
           <a href="/courses/JS_DAO">
-            <Button id="wish-to-sign-in" ref={ref}>
-              Quero me inscrever!
+            <Button id="wish-to-sign-in" color={'success'} ref={ref}>
+              <Text weight={'extrabold'} >Quero me inscrever!</Text>
             </Button>
           </a>
         </div>
       </div>
-      <div className='lg:m-0 mx-auto'>
-      <Cover />
-      </div>
-    </main>
+      
+    </div>
   )
 }
