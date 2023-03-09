41379e78a4d4222dfd992e5506f4add99d8bef11
vitordev <vitor.mancio@gmail.com>
2023-03-09 17:49:42 -0300

    [fix/feat]Change home to Solana(next build projext) and improve ui design layout

--- components/Home/Cover/index.js
+++ components/Home/Cover/index.js
@@ -3,13 +3,11 @@ import React from 'react'
 
 export default function Cover() {
   return (
-    <div>
       <Image
         width={400}
         height={400}
         src="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FJS_DAO.png?alt=media&token=3778f188-89dc-48c7-b557-50ae36732436"
-        className="flex-1 rounded-3xl"
+        className="rounded-3xl"
       />
-    </div>
   )
 }
