8e164fd6dc5669e972a62f138006cf6b37334b59
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-04-18 16:54:52 -0300

    update cover to GPT3_Writer img

--- components/Home/Cover/index.js
+++ components/Home/Cover/index.js
@@ -6,7 +6,7 @@ export default function Cover() {
       <Image
         width={400}
         height={400}
-      src="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_And_Web3.png?alt=media&token=c10c7dc7-2910-4028-83fc-2fc3df3a961f"
+      src="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FGPT3_Writer.png?alt=media&token=ebc653dd-64b1-4647-80e9-cc9cc4c85463"
         className="rounded-3xl"
       />
   )
