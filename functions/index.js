e98c2e0b2387cc7ef0fb39a541d15f9809d2bdee
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-04-19 19:31:40 -0300

    [feat] minting missing nfts from big query results

--- functions/index.js
+++ functions/index.js
@@ -148,6 +148,48 @@ exports.mintAllMissing = functions
     resp.send('ok')
   })
 
+exports.mintMissingFromBigQuery = functions.runWith({ timeoutSeconds: 540 }).https.onRequest(async(req, resp) => {
+    // Import the Google Cloud client library using default credentials
+    const {BigQuery} = require('@google-cloud/bigquery');
+    const bigquery = new BigQuery();
+
+    const query = `select c.course_id, l.cohort_id, l.user_id
+    from \`web3dev-bootcamp.web3dev_bootcamp.lesson_submissions\` l 
+    join \`web3dev-bootcamp.web3dev_bootcamp.cohorts\` c on c.id = l.cohort_id
+    join \`web3dev-bootcamp.web3dev_bootcamp.cohorts\` c2 on c.course_id = c2.course_id and c2.name = 'ETERN@S' 
+    left join \`web3dev-bootcamp.web3dev_bootcamp.nft_mints\` n on n.user_id = l.user_id and (n.cohort_id = l.cohort_id or n.cohort_id = c2.id)
+     where (lesson = 'Lesson_2_Finalize_Celebrate.md' or lesson = 'Lesson_2_Ship_It.md' or lesson = 'Lesson_2_Finalize_And_Celebrate.md' or lesson = 'Lesson_2_Finishing_Touches_Contract.md')
+    and n.user_id is null`;
+
+    const options = {
+      query: query,
+      location: 'US',
+    };
+
+    // Run the query as a job
+    const [job] = await bigquery.createQueryJob(options);
+    console.log(`Job ${job.id} started.`);
+
+    // Wait for the query to finish
+    const [rows] = await job.getQueryResults();
+
+    Promise.all(rows.map(async (row) => {
+      let cohort = {
+        id: row.cohort_id,
+      };
+      try {
+        console.log('minting for user ' + row.user_id + ' cohort ' + row.cohort_id);
+        await issueCertificate(row.user_id, cohort);
+      } catch (e) {
+        console.log('error: ' + e);
+      }
+    }));
+
+    console.log('done')
+    resp.send('ok')
+    return;
+});
+
 exports.sendEmailJob = functions.pubsub.topic('course_day_email').onPublish((message) => {
   const data = JSON.parse(Buffer.from(message.data, 'base64'))
 
