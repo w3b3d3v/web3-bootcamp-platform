7335e5decd7f882df4180c967a02387f434cd9c1
Daniel Cukier <danicuki@gmail.com>
2023-05-10 15:21:46 -0300

    remove region paramenter

--- functions/lib/bigQuery.js
+++ functions/lib/bigQuery.js
@@ -1,10 +1,8 @@
 const getQueryResults = async function (query) {
   const { BigQuery } = require('@google-cloud/bigquery')
   const bigquery = new BigQuery()
-
   const options = {
     query: query,
-    location: 'US',
   }
 
   const [job] = await bigquery.createQueryJob(options)
