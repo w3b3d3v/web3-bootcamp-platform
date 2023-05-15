203385b6556e4daafada5d0a48187e05916e6ff8
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-15 17:55:47 -0300

    [feat] receive imageUrl as property

--- components/Home/Cover/index.js
+++ components/Home/Cover/index.js
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import React from 'react'
 
-export default function Cover() {
+export default function Cover(props) {
   return (
       <Image
         width={400}
         height={400}
-      src="https://firebasestorage.googleapis.com/v0/b/web3dev-bootcamp.appspot.com/o/courses_cover%2FSolana_NFTs.png?alt=media&token=6562a486-7682-4c64-9bfd-a33c26d82199"
+      src={props.imageUrl}
         className="rounded-3xl"
       />
   )
