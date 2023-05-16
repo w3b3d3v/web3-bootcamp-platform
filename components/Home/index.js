ab55fd3a98241e21b674c1562e83d551101245e0
Daniel Cukier <danicuki@gmail.com>
2023-05-16 14:26:19 -0300

    fix null course bug

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,58 +1,53 @@
 import React from 'react'
 import { Text, Button } from '@nextui-org/react'
-import { getHomeCourse } from '../../lib/course'
+import { getHomeCourse, defaultCourse } from '../../lib/course'
 import Cover from './Cover'
 
 export default function Main() {
   const ref = React.createRef()
-  const [course, setCourse] = React.useState(null);
-  const [link, setLink] = React.useState(null);
+  const [course, setCourse] = React.useState(null)
+  const [link, setLink] = React.useState(null)
 
   React.useEffect(() => {
     getHomeCourse().then((course) => {
-      setCourse(course);
-    });
-  }, []);
+      setCourse(course)
+    })
+  }, [])
 
-  React.useEffect(() => {
+  React.useMemo(() => {
     if (course) {
-      setLink(`/courses/${course.id}`);
+      setLink(`/courses/${course.id}`)
     }
-  }, [course]);
+  }, [course])
 
-  return (
-    <div className="max-w-7xl mx-auto mb-14 px-8 mt-4 flex flex-col justify-between lg:mt-16 lg:flex-row items-center gap-0 lg:gap-52 z-0">
-      <div className="flex flex-col items-center mx-auto flex-1">
+  return course == null ? (
+    ''
+  ) : (
+    <div className="z-0 mx-auto mb-14 mt-4 flex max-w-7xl flex-col items-center justify-between gap-0 px-8 lg:mt-16 lg:flex-row lg:gap-52">
+      <div className="mx-auto flex flex-1 flex-col items-center">
         <div>
-          <div className='max-w-lg m-auto' >
-            <Text h1 auto className="text-center font-bold mb-4">
-              Crie uma coleção de NFTs em{' '}
-              <span className="lp-gradient-text bg-gradient-to-r from-green-400 to-teal-400"></span>{' '}
-              <span className="lp-gradient-text bg-gradient-to-r from-teal-400 to-fuchsia-500 ">
-                Solana{' '}
-              </span>
+          <div className="m-auto max-w-lg">
+            <Text h1 auto className="mb-4 text-center font-bold">
+              {course.title}
             </Text>
           </div>
-          <div className='flex justify-center mb-7 text-justify'>
-            <Cover imageUrl={course.image_url}/>
+          <div className="mb-7 flex justify-center text-justify">
+            <Cover imageUrl={course.image_url} />
           </div>
           <div className="max-w-2xl">
-            <Text>
-            { course?.description }
-            </Text>
-            <br/>
+            <Text>{course?.description}</Text>
+            <br />
           </div>
         </div>
         <br />
-        <div className="lg:self-start m-auto mb-8 z-0">
+        <div className="z-0 m-auto mb-8 lg:self-start">
           <a href={link}>
             <Button id="wish-to-sign-in" color={'success'} ref={ref}>
-              <Text weight={'extrabold'} >Quero me inscrever!</Text>
+              <Text weight={'extrabold'}>Quero me inscrever!</Text>
             </Button>
           </a>
         </div>
       </div>
-      
     </div>
   )
 }
