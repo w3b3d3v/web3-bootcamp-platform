75a31e0634f9382e41e05c6dd1a9213ed8368638
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-17 15:21:51 -0300

    [fix] fix object accessing on view

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -14,7 +14,6 @@ export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort })
   useEffect(async () => {
     setBuildAnalytics(await getCourseAnalytics(course.id))
   }, [course])
-
   const toggle = (tab) => {
     if (activeTab !== tab) setActiveTab(tab)
   }
@@ -25,7 +24,7 @@ export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort })
         {Object.keys(getCourse?.sections)
           .sort()
           .map((section) => {
-            let sectionAnalytics = buildAnalytics?.sections?._field_1?.find((item) => item.section === section);
+            let sectionAnalytics = buildAnalytics?.sections?.find((item) => item.section === section);
             return (
               <div className="flex justify-between flex-col" key={section}>
                 {isLessonPage ? (
