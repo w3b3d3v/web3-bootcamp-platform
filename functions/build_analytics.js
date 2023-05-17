c81f78f93d90e85e2071dcd25aa56ca2a3655dd8
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-17 15:01:36 -0300

    [fix] new query.2

--- functions/build_analytics.js
+++ functions/build_analytics.js
@@ -2,31 +2,40 @@ const { getQueryResults } = require('./lib/bigQuery')
 
 async function usersBySection() {
   return getQueryResults(`
-  WITH sections AS (
+  WITH max_section AS (
+    SELECT l.user_id, l.cohort_id, MAX(l.section) AS max_section
+    FROM web3dev-bootcamp.web3dev_bootcamp.lesson_submissions l
+    GROUP BY l.user_id, l.cohort_id
+  ),
+  section_data AS (
     SELECT
       c.course_id,
-      section,
-      COUNT(l.user_id) AS students,
+      m.max_section,
+      COUNT(DISTINCT m.user_id) AS students,
       ARRAY_AGG(u.photoUrl IGNORE NULLS LIMIT 3) AS photoUrls
-    FROM web3dev-bootcamp.web3dev_bootcamp.lesson_submissions l
-    JOIN web3dev-bootcamp.web3dev_bootcamp.cohorts c ON c.id = l.cohort_id
-    JOIN web3dev-bootcamp.web3dev_bootcamp.users u ON u.id = l.user_id
-    GROUP BY 1, 2
+    FROM max_section m
+    JOIN web3dev-bootcamp.web3dev_bootcamp.cohorts c ON c.id = m.cohort_id
+    JOIN web3dev-bootcamp.web3dev_bootcamp.users u ON u.id = m.user_id
+    GROUP BY c.course_id, m.max_section
   )
   SELECT
     course_id,
-    STRUCT(
-      ARRAY_AGG(
-        STRUCT(
-          section,
-          students,
-          photoUrls
-        ) ORDER BY section
+    (SELECT
+      STRUCT(
+        ARRAY_AGG(
+          STRUCT(
+            CAST(max_section AS STRING) AS section,
+            students,
+            photoUrls
+          )
+        ) AS sections
       )
-    ) AS sections
-  FROM sections
+     FROM section_data sd
+     WHERE sd.course_id = cd.course_id
+    ).sections AS sections
+  FROM section_data cd
   GROUP BY course_id
-  ORDER BY course_id;
+  ORDER BY course_id;  
   `)
 }
 
