a6e9ec582e14a4629a2b5dcaf1cac6b23e3fa410
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-05-29 17:13:44 -0300

    [feat] new functions to check existence of user

--- functions/orbit.js
+++ functions/orbit.js
@@ -7,8 +7,8 @@ const HEADERS = {
     Authorization: `Bearer ${process.env.ORBIT_API_KEY}`,
 }
 
-async function insertMember(member) {
-    let buildMember = formatMember(member);
+async function insertMember(user) {
+    let buildMember = formatUserToMember(user);
 
     try {
         await axios.post(ORBIT_API_URL + "members", buildMember, { headers: HEADERS });
@@ -20,7 +20,43 @@ async function insertMember(member) {
     }
 }
 
-function formatMember(member) {
+async function findMemberByEmail(email) {
+    try {
+        let response = await axios.get(ORBIT_API_URL + "members/find?source=email&email=" + email, { headers: HEADERS });
+        return response.data.data;
+    }
+    catch(error) {
+        console.error("Error finding member: ", error);
+        return null;
+    }
+}
+
+async function updateMemberIdentity(user, member_slug) {
+    try {
+        let formatedIdentity = formatIdentity(user);
+        let response = await axios.get(
+            ORBIT_API_URL + `members/${member_slug}/identities`, formatedIdentity, { headers: HEADERS }
+        );
+        return response.data.data;
+    }
+    catch(error) {
+        console.error("Error updating member identity: ", error);
+        return null;
+    }
+}
+
+function formatIdentity(user) {
+    return {
+        "name": "Web3dev",
+        "source": "web3devBuild",
+        "source_host": "https://bootcamp.web3dev.com.br/",
+        "username": user.username,
+        "uid": user.id,
+        "email": user.email,
+    }
+}
+
+function formatUserToMember(member) {
     return {
         "member": {
             "name": member.username,
@@ -37,4 +73,4 @@ function formatMember(member) {
     }
 }
 
-module.exports = { insertMember }
+module.exports = { insertMember, findMemberByEmail, updateMemberIdentity }
