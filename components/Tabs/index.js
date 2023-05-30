11c2259685f3933bc94da1976cf33be840ec4b7b
Daniel Cukier <danicuki@gmail.com>
2023-05-30 14:56:32 -0300

    refactor to use courses collection instead of course_analytics

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import useAuth from '../../hooks/useAuth'
 import { checkSections, colorTab } from './tabFunctions'
-import { getCourseAnalytics } from '../../lib/courses'
 
 export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort }) {
   const getCourse = course
@@ -9,40 +8,42 @@ export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort })
   if (!getCourse?.sections) return null
   // list all the section as tabs
   const [activeTab, setActiveTab] = useState(Object.keys(getCourse?.sections).sort())
-  const [buildAnalytics, setBuildAnalytics] = useState();
 
-  useEffect(async () => {
-    setBuildAnalytics(await getCourseAnalytics(course.id))
-  }, [course])
   const toggle = (tab) => {
     if (activeTab !== tab) setActiveTab(tab)
   }
 
   return (
-    <div className="flex flex-col rounded-lg p-4 shadow-xl lg:mt-12 mt-6 mb-6">
+    <div className="mt-6 mb-6 flex flex-col rounded-lg p-4 shadow-xl lg:mt-12">
       <div className="flex flex-row justify-between maxsm:flex-col maxsm:text-center">
         {Object.keys(getCourse?.sections)
           .sort()
           .map((section) => {
-            let sectionAnalytics = buildAnalytics?.sections?.find((item) => item.section === section);
+            let sectionAnalytics = course.analytics?.find((item) => item.section === section)
             return (
-              <div className="flex justify-between flex-col" key={section}>
+              <div className="flex flex-col justify-between" key={section}>
                 {isLessonPage ? (
                   <div
-                    className={`item flex-grow rounded px-4 py-2 font-bold shadow-lg hover:no-underline mb-3 ${colorTab(
-                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort).isSectionCompleted,
-                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort).currentSection
+                    className={`item mb-3 flex-grow rounded px-4 py-2 font-bold shadow-lg hover:no-underline ${colorTab(
+                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort)
+                        .isSectionCompleted,
+                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort)
+                        .currentSection
                     )}`}
                   >
-                    <p className="m-0 p-0 text-center center">{section?.replace('Section_', 'Seção ')}</p>
+                    <p className="center m-0 p-0 text-center">
+                      {section?.replace('Section_', 'Seção ')}
+                    </p>
                   </div>
                 ) : (
                   <a
                     href={`#${!isLessonPage ? section : ''}`}
                     onClick={() => toggle(section)}
-                    className={`item flex-grow rounded px-4 py-2 font-bold shadow-lg hover:no-underline b-3 ${colorTab(
-                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort).isSectionCompleted,
-                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort).currentSection
+                    className={`item b-3 flex-grow rounded px-4 py-2 font-bold shadow-lg hover:no-underline ${colorTab(
+                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort)
+                        .isSectionCompleted,
+                      checkSections(course, lessonsSubmitted, section, user?.uid, cohort)
+                        .currentSection
                     )}`}
                   >
                     <p className="m-0 p-0">{section?.replace('Section_', 'Seção ')}</p>
@@ -50,31 +51,33 @@ export default function Tabs({ course, isLessonPage, lessonsSubmitted, cohort })
                 )}
                 {sectionAnalytics && (
                   <div className="flex flex-col items-center">
-                  <p className="m-0 p-0 mt-2" style={{ fontSize: "14px" }}>{sectionAnalytics?.students} pessoas construindo nessa seção!</p>
-                  <div className="flex flex-row ml-2 items-center">
-                    {sectionAnalytics?.photoUrls.slice(0, 3).map((source, index) => (
-                      <img
-                        key={source}
-                        src={source}
-                        alt="User avatar"
-                        style={{
-                          width: '50px',
-                          height: '50px',
-                          borderRadius: '50%',
-                          marginLeft: index !== 0 ? '-25px' : '15px',
-                          marginTop: '1rem',
-                          marginBottom: '1rem',
-                          zIndex: sectionAnalytics?.photoUrls.length - index,
-                        }}
-                      />
-                    ))}
+                    <p className="m-0 mt-2 p-0" style={{ fontSize: '14px' }}>
+                      {sectionAnalytics?.students} pessoas construindo nessa seção!
+                    </p>
+                    <div className="ml-2 flex flex-row items-center">
+                      {sectionAnalytics?.photoUrls.slice(0, 3).map((source, index) => (
+                        <img
+                          key={source}
+                          src={source}
+                          alt="User avatar"
+                          style={{
+                            width: '50px',
+                            height: '50px',
+                            borderRadius: '50%',
+                            marginLeft: index !== 0 ? '-25px' : '15px',
+                            marginTop: '1rem',
+                            marginBottom: '1rem',
+                            zIndex: sectionAnalytics?.photoUrls.length - index,
+                          }}
+                        />
+                      ))}
+                    </div>
                   </div>
-                </div>  
                 )}
               </div>
             )
           })}
       </div>
     </div>
-  );  
+  )
 }  
--- functions/build_analytics.js
+++ functions/build_analytics.js
@@ -2,10 +2,10 @@ const { getQueryResults } = require('./lib/bigQuery')
 
 async function usersBySection() {
   return getQueryResults(`
-  WITH max_section AS (
+WITH max_section AS (
     SELECT l.user_id, c.course_id, MAX(l.section) AS max_section
-    FROM web3dev-bootcamp.web3dev_bootcamp.lesson_submissions l
-    JOIN web3dev-bootcamp.web3dev_bootcamp.cohorts c ON c.id = l.cohort_id
+    FROM web3dev_bootcamp.lesson_submissions l
+    JOIN web3dev_bootcamp.cohorts c ON c.id = l.cohort_id
     GROUP BY l.user_id, c.course_id
   ),
   section_data AS (
@@ -15,13 +15,12 @@ async function usersBySection() {
       COUNT(DISTINCT m.user_id) AS students,
       ARRAY_AGG(u.photoUrl IGNORE NULLS LIMIT 3) AS photoUrls
     FROM max_section m
-    JOIN web3dev-bootcamp.web3dev_bootcamp.users u ON u.id = m.user_id
+    JOIN web3dev_bootcamp.users u ON u.id = m.user_id
     GROUP BY m.course_id, m.max_section
   )
   SELECT
     course_id,
     (SELECT
-      STRUCT(
         ARRAY_AGG(
           STRUCT(
             CAST(max_section AS STRING) AS section,
@@ -29,30 +28,21 @@ async function usersBySection() {
             photoUrls
           )
         ) AS sections
-      )
      FROM section_data sd
      WHERE sd.course_id = cd.course_id
-    ).sections AS sections
+    ) AS sections
   FROM section_data cd
   GROUP BY course_id
   ORDER BY course_id;  
-  `)
+`)
 }
 
 async function storeUsersPerCohort(db, rows) {
-  const analyticsRef = db.collection('builds_analytics')
   const promises = rows.map(async (item) => {
     const { course_id, sections } = item
-    const querySnapshot = await analyticsRef.where('course_id', '==', course_id).get()
-    if (querySnapshot.empty) {
-      const documentRef = analyticsRef.doc()
-      await documentRef.set({ course_id, sections })
-      return { course_id, result: 'created' }
-    } else {
-      const documentRef = querySnapshot.docs[0].ref
-      await documentRef.update({ sections })
-      return { course_id, result: 'updated' }
-    }
+    const course = db.collection('courses').doc(course_id)
+    await course.update({ analytics: sections })
+    return { course_id, result: 'updated' }
   })
   await Promise.all(promises)
 }
