32ec564f263ebb13398460253b9772214b190736
Lorenzobattistela <lorenzowottrich@gmail.com>
2023-06-28 09:13:38 -0300

    [test] bigquery calling

--- functions/test/lib/bigQuery.test.js
+++ functions/test/lib/bigQuery.test.js
@@ -1,27 +1,17 @@
 const { getQueryResults } = require('../../lib/bigQuery');
-const nock = require('nock');
+let record = require('../record');
 
-describe('getQueryResults', () => {
-    jest.setTimeout(30000);
-    beforeEach(() => {
-      nock('https://bigquery.googleapis.com')
-        .post('/bigquery/v2/projects/your-project-id/queries')
-        .reply(200, {
-          message : {
-            response: "success"
-          },
-        });
-    });
+require('dotenv').config()
+
+describe('getQueryResults', function() {
+  var recorder = record('bigQuery')
+  beforeAll(recorder.before);
+
+  it('should return query results', async () => {
+    const query = 'SELECT * FROM web3dev_bootcamp.users LIMIT 1';
+    const res = await getQueryResults(query);
+    expect(res).toEqual([[{"bio": "", "blockchainExp": null, "builder": [], "cohorts": [], "createdAt": {"value": "2022-05-01T00:00:00.000Z"}, "devExp": null, "discord": null, "discord_id": null, "email": "alan.franzin2011@gmail.com", "github": null, "id": "23WEoArqzRf4ORh4cdvadaVsYtj1", "linkedin": null, "name": null, "personalWebsite": null, "photoUrl": null, "referred_by": null, "technologies": [], "twitter": null, "wallet": null}]]);
+  }, 50000)
   
-    afterEach(() => {
-      nock.cleanAll();
-    });
-  
-    it('should return query results', async () => {
-      const query = 'SELECT * FROM your_table';
-      const results = await getQueryResults(query);
-      expect(results).toEqual({message : {
-        response: "success"
-      }});
-    });
-  });
+  afterAll(recorder.after);
+})
