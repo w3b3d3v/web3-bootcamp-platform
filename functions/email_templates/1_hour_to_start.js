63694638c1b9c955cae6b8aba0c75c048d722910
Vini Black <viniibl4ck@gmail.com>
2024-03-04 16:20:22 -0300

    fix: adding correct placeholder

--- functions/email_templates/1_hour_to_start.js
+++ functions/email_templates/1_hour_to_start.js
@@ -1,10 +1,10 @@
 function template(params) {
   return `<!DOCTYPE html>Fala builder 👷👷‍!!<div><br></div>
-  <div>Faltam poucos minutos ⌛ para o lançamento do build {nome do build} (19h horário Brasília).
+  <div>Faltam poucos minutos ⌛ para o lançamento do build ${params.course.title} (19h horário Brasília).
     <div><br></div>
     Participe ao vivo da transmissão que será <a href="https://youtube.com/web3dev">pelo nosso Youtube</a>, assim você pode tirar todas as suas dúvidas iniciais.
   <div><br></div>
-  <div>Ah! Não esquece de entrar no <a href="https://discord.gg/web3dev">nosso Discord</a>
+  <div>Ah! Não esquece de entrar no <a href="https://pt.discord.w3d.community">nosso Discord</a>
     <div><br></div>
     A sua turma já está lá pra codar esse projeto com você. 😀</div>
   <div><br></div>
