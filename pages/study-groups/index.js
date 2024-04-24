2cda0bbd5711bca23f644f9664bbdbda785e39c9
Yan Luiz <yan@nomadbitcoin.xyz>
2024-04-24 19:04:49 -0300

    feat: Cards and Head Description added

---
+++ pages/study-groups/index.js
@@ -0,0 +1,81 @@
+import Head from 'next/head'
+import { useState } from 'react'
+import { getAllGroups } from '../../lib/study-groups'
+import { Container } from '@nextui-org/react'
+import { GroupCard } from '../../components/Card/Group'
+import Image from 'next/image'
+ 
+function Groups({ AllGroups }) {
+
+  const [showMore, setShowMore] = useState(false)
+  const course = {
+    "title": "List of All Groups",
+    "description": "Crie sua própria loja virtual na Web3 com Solana Pay Nesse desafio você irá construir uma loja para vender suas utilidades ou artes na blockchain, sem precisar usar Rust. A Solana é uma das redes blockchains mais completas do ecossistema e possui toda infraestrutura para você construir uma loja nesta rede e receber pagamentos em criptomoedas. Se você estava esperando uma oportunidade para empreender na web3, a hora é agora! Esse bootcamp vai conduzir você passo a passo na criação desta loja sem precisar conhecer Rust, usando apenas JavaScript, Next.js e conhecimentos básicos de programação.",
+    "image_url":"/study_groups_resized.png"
+  }
+
+  const styleImageCover = {
+    borderRadius:'10px'
+  }
+
+  return (
+    <>
+      <Head>
+        <meta property="og:title" content="Listagem" />
+        <title>Lista de Builds - WEB3DEV</title>
+      </Head>
+
+      <div className="container-lessons mx-auto mt-0 max-w-7xl px-6 lg:mt-10">
+        <div className="mb-8 flex flex-col justify-between lg:flex-row">
+          <div className="max-w-3xl self-center lg:max-w-lg">
+            <h1 className="text-2xl font-bold">{course?.title}</h1>
+            <p className="mb-6  text-sm">{course?.description /*.substring(0, 100) + '...'*/}</p>
+          </div>
+          <div className="mx-auto h-full lg:mx-0">
+            <Image
+              src={course?.image_url}
+              width="300px"
+              height="300px"
+              style={styleImageCover}
+            ></Image>
+          </div>
+        </div>
+      </div>
+
+      <Container
+        css={{
+          mt: 30,
+          mb: 30,
+        }}
+      >
+        <Container
+          css={{
+            display: 'flex',
+            gap: '$10',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {AllGroups.map((g) => {
+            return (
+              <>
+                <GroupCard group={g} />
+              </>
+            )
+          })}
+        </Container>
+      </Container>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const AllGroups = await getAllGroups()
+  return {
+    props: {
+      AllGroups,
+    },
+  }
+}
+
+export default Groups
