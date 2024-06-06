7efbd4beb4f8579ddad1add7ecdf57d31f00c7e2
Daniel Cukier <danicuki@gmail.com>
2024-06-06 10:27:29 -0300

    fix translation

--- components/Card/ComingSoon/index.js
+++ components/Card/ComingSoon/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function ComingSoonCard() {
+  const { t } = useTranslation()
   return (
     <>
-        <div className="rounded-lg bg-white-100 shadow-xl dark:bg-black-200 min-w-full">
-          <div className="flex min-w-full">
-            <div className="px-6 py-5 min-w-full items-center flex justify-between">
-              <div className='flex-col'>
+      <div className="min-w-full rounded-lg bg-white-100 shadow-xl dark:bg-black-200">
+        <div className="flex min-w-full">
+          <div className="flex min-w-full items-center justify-between px-6 py-5">
+            <div className="flex-col">
               <p className="text-base font-medium leading-none text-black-200 dark:text-gray-100">
-              &#x1F98F; Está chegando a hora!
+                &#x1F98F; {t('comming_soon.timeHasCome')}
               </p>
               <p className="pt-2 text-xs leading-5 text-gray-500 dark:text-gray-400">
-                Estamos finalizando os últimos preparativos para o evento. Volte para conferir em breve!
+                {t('comming_soon.preparingEvent')}
               </p>
-              </div>
             </div>
           </div>
         </div>
+      </div>
     </>
   )
 }
