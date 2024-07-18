ae2baea22c728b45f736f9682a65461007cb19f8
Yan Luiz <yan@nomadbitcoin.xyz>
2024-07-18 12:53:53 -0300

    lint

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -1,53 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
-import { withProtected } from '../../hooks/route';
-import { db } from '../../firebase/initFirebase';
-import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { withProtected } from '../../hooks/route'
+import { db } from '../../firebase/initFirebase'
+import { collection, getDocs } from 'firebase/firestore'
 
 const IssueCard = ({ issue }) => {
   return (
-    <div className="border border-gray-200 rounded-lg p-4 m-2 shadow-lg bg-gray-100 flex flex-col justify-between">
+    <div className="m-2 flex flex-col justify-between rounded-lg border border-gray-200 bg-gray-100 p-4 shadow-lg">
       <div className="mb-4">
         <h3 className="text-lg text-indigo-500">{issue.title}</h3>
         <p className="text-sm text-indigo-500">{issue.project_name}</p>
       </div>
-      <p className="text-sm text-gray-700 mb-4">{issue.body}</p>
+      <p className="mb-4 text-sm text-gray-700">{issue.body}</p>
       <div className="text-sm text-gray-700">
-        <p><strong>Status:</strong> {issue.state}</p>
-        <p><strong>Created At:</strong> {new Date(issue.createdAt).toLocaleDateString()}</p>
-        <p><strong>Updated At:</strong> {new Date(issue.updatedAt).toLocaleDateString()}</p>
+        <p>
+          <strong>Status:</strong> {issue.state}
+        </p>
+        <p>
+          <strong>Created At:</strong> {new Date(issue.createdAt).toLocaleDateString()}
+        </p>
+        <p>
+          <strong>Updated At:</strong> {new Date(issue.updatedAt).toLocaleDateString()}
+        </p>
       </div>
-      <div className="text-sm text-gray-700 bg-gray-200 rounded-lg p-2 mt-4">
-        {issue.fields && issue.fields.length > 0 && (
+      <div className="mt-4 rounded-lg bg-gray-200 p-2 text-sm text-gray-700">
+        {issue.fields &&
+          issue.fields.length > 0 &&
           issue.fields.map((field, index) => (
-            <p key={index}><strong>{field.field}:</strong> {field.value}</p>
-          ))
-        )}
+            <p key={index}>
+              <strong>{field.field}:</strong> {field.value}
+            </p>
+          ))}
         {issue.assignees && issue.assignees.length > 0 && (
-          <p><strong>Assignees:</strong> {issue.assignees.map((assignee, index) => (
-            <span key={index} className="block">{assignee}</span>
-          ))}</p>
+          <p>
+            <strong>Assignees:</strong>{' '}
+            {issue.assignees.map((assignee, index) => (
+              <span key={index} className="block">
+                {assignee}
+              </span>
+            ))}
+          </p>
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
 const Sidebar = ({ filters, setFilters }) => {
   const handleFilterChange = (e) => {
     setFilters({
       ...filters,
       [e.target.name]: e.target.value,
-    });
-  };
+    })
+  }
 
   return (
-    <div className="w-full sm:w-64 p-4 text-white">
-      <h3 className="text-xl mb-4">Filtros</h3>
+    <div className="text-white w-full p-4 sm:w-64">
+      <h3 className="mb-4 text-xl">Filtros</h3>
       <div className="mb-4">
         <label>
-          <span className="block mb-2">Status:</span>
-          <select name="status" onChange={handleFilterChange} className="w-full p-2 rounded-lg bg-gray-800 text-white">
+          <span className="mb-2 block">Status:</span>
+          <select
+            name="status"
+            onChange={handleFilterChange}
+            className="text-white w-full rounded-lg bg-gray-800 p-2"
+          >
             <option value="">Todos</option>
             <option value="OPEN">Open</option>
             <option value="CLOSED">Closed</option>
@@ -55,32 +72,30 @@ const Sidebar = ({ filters, setFilters }) => {
         </label>
       </div>
     </div>
-  );
-};
+  )
+}
 
 const Profile = () => {
-  const [issues, setIssues] = useState([]);
-  const [filters, setFilters] = useState({});
+  const [issues, setIssues] = useState([])
+  const [filters, setFilters] = useState({})
 
   useEffect(() => {
     const fetchIssues = async () => {
       try {
-        const querySnapshot = await getDocs(collection(db, "tasks"));
-        const issuesList = querySnapshot.docs.map(doc => doc.data());
-        setIssues(issuesList);
+        const querySnapshot = await getDocs(collection(db, 'tasks'))
+        const issuesList = querySnapshot.docs.map((doc) => doc.data())
+        setIssues(issuesList)
       } catch (error) {
-        console.error('Error fetching issues:', error);
+        console.error('Error fetching issues:', error)
       }
-    };
+    }
 
-    fetchIssues();
-  }, []);
+    fetchIssues()
+  }, [])
 
-  const filteredIssues = issues.filter(issue => {
-    return (
-      (!filters.status || issue.state === filters.status)
-    );
-  });
+  const filteredIssues = issues.filter((issue) => {
+    return !filters.status || issue.state === filters.status
+  })
 
   return (
     <>
@@ -93,8 +108,8 @@ const Profile = () => {
           {filteredIssues.length === 0 ? (
             <p>Nenhuma issue encontrada.</p>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
-              {filteredIssues.map(issue => (
+            <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+              {filteredIssues.map((issue) => (
                 <IssueCard key={issue.github_id} issue={issue} />
               ))}
             </div>
@@ -102,7 +117,7 @@ const Profile = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default withProtected(Profile);
+export default withProtected(Profile)
