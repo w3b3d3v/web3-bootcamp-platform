9d12dd386f2290d9378c22bc6226d85337f34c2d
Andre Silva <hardsys_andre@hotmail.com>
2024-07-25 11:49:28 -0300

    Background component adjustment and language adjustment in codes

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -4,51 +4,52 @@ import { withProtected } from '../../hooks/route'
 import { getAllTasks } from '../../lib/tasks'
 import { useTranslation } from 'react-i18next'
 import SearchBar from '../../components/SearchBar'
-import { MdGroup } from "react-icons/md"
-import { AiOutlineLike } from "react-icons/ai"
+import { MdGroup } from 'react-icons/md'
+import { AiOutlineLike } from 'react-icons/ai'
 import { Button, Image, Text } from '@nextui-org/react'
-import Filter from '../../components/FiltroTasks/index'
+import Filter from '../../components/FilterTasks/index'
 import { useTheme } from 'next-themes'
-
-
 
 const IssueCard = ({ issue }) => {
   const { t } = useTranslation()
   const { theme } = useTheme()
   const isLight = theme === 'light'
   return (
-    <div className={`flex flex-row gap-2 rounded-lg p-4 shadow-lg ${
-      isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
-    }`}>
-      <div className='flex items-center justify-center rounded-[20px] bg-[#99e24d] w-[100px] h-full'>
+    <div
+      className={`flex flex-row gap-2 rounded-lg p-4 shadow-lg ${
+        isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
+      }`}
+    >
+      <div className="flex h-full w-[100px] items-center justify-center rounded-[20px] bg-[#99e24d]">
         <MdGroup size={80} color="white" />
       </div>
-      <div className="flex flex-col mb-4 w-full gap-3">
-        <div className='flex w-full justify-between items-center'>
+      <div className="mb-4 flex w-full flex-col gap-3">
+        <div className="flex w-full items-center justify-between">
           <span className="text-[24px] text-[#99e24d]">{issue.title}</span>
-          <button className="mr-4 px-2 py-1 text-sm text-white bg-[#99e24d] bg-opacity-30 rounded hover:bg-[#649e26] focus:outline-none focus:ring-2 focus:ring-[#99e24d]">
+          <button className="text-white mr-4 rounded bg-[#99e24d] bg-opacity-30 px-2 py-1 text-sm hover:bg-[#649e26] focus:outline-none focus:ring-2 focus:ring-[#99e24d]">
             Apply
           </button>
         </div>
-        <div className='flex w-full'>
-          <p className='text-[16px]'>
-          Our practical learning platform, located at build.w3d.community, offers courses focused on blockchain and web3 technologies.
+        <div className="flex w-full">
+          <p className="text-[16px]">
+            Our practical learning platform, located at build.w3d.community, offers courses focused
+            on blockchain and web3 technologies.
           </p>
         </div>
         <div className="flex flex-row gap-3 text-gray-400">
-          <p className='text-[14px]'>
+          <p className="text-[14px]">
             <strong>Board:</strong> {issue.project_name}
           </p>
-          <p className='text-[14px]'>
+          <p className="text-[14px]">
             <strong>Created At:</strong> {new Date(issue.createdAt).toLocaleDateString()}
           </p>
-          <p className='text-[14px]'>
+          <p className="text-[14px]">
             <strong>Updated At:</strong> {new Date(issue.updatedAt).toLocaleDateString()}
           </p>
-          <p className='text-[14px]'>
+          <p className="text-[14px]">
             <strong>Skill:</strong> {new Date(issue.updatedAt).toLocaleDateString()}
           </p>
-          <p className='text-[14px]'>
+          <p className="text-[14px]">
             <strong>Reward:</strong> {new Date(issue.updatedAt).toLocaleDateString()}
           </p>
         </div>
@@ -66,7 +67,6 @@ const Sidebar = ({ filters, setFilters }) => {
       [e.target.name]: e.target.value,
     })
   }
-
   return (
     <div className="text-white w-full p-4 sm:w-64">
       <h3 className="mb-4 text-xl">{t('filters')}</h3>
@@ -102,20 +102,19 @@ const Sortbar = ({ filters, setFilters }) => {
       [e.target.name]: e.target.value,
     })
   }
-
   return (
-    <div className="text-white w-full sm:w-64 h-10">
-      
-      <div className={`flex flex-row gap-1 items-center rounded-lg w-[200px] justify-center ${
-                    isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
-                    } `}>
-      
-      <span className=" text-[14px]">{t('Sort by')}</span>
+    <div className="text-white h-10 w-full sm:w-64">
+      <div
+        className={`flex w-[200px] flex-row items-center justify-center gap-1 rounded-lg ${
+          isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
+        } `}
+      >
+        <span className=" text-[14px]">{t('Sort by')}</span>
         <label>
           <select
             name="status"
             onChange={handleFilterChange}
-            className="text-white w-full text-[#99e24d] border-none bg-none text-[14px]"
+            className="text-white w-full border-none bg-none text-[14px] text-[#99e24d]"
           >
             <option value="">{t('context depth')}</option>
             <option value="">{t('reward')}</option>
@@ -133,74 +132,63 @@ const TaskPage = ({ issues }) => {
   const [filters, setFilters] = useState({})
   const [searchQuery, setSearchQuery] = useState('')
 
-  
-
   const filteredIssues = issues.filter((issue) => {
     return !filters.status || issue.state === filters.status
-    
   })
-
   return (
     <>
       <Head>
         <title> Tasks - WEB3DEV</title>
       </Head>
-      <div className='flex items-center justify-center w-full'>
-      <div className='flex flex-col w-[80%]' >
-        <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
-        <div className='flex'>
-          <div className="flex flex-col sm:flex-row w-[100%] items-start ">
-            <Filter />
-
-
-
-
-            {/*<Sidebar filters={filters} setFilters={setFilters} t={t} />*/}
-            <div className="flex-1 p-4 ">
-              {filteredIssues.length === 0 ? (
-                <p>{t('no-issues-found')}.</p>
-              ) : (
-                <div className='flex flex-col gap-2'>
-                  <div className='flex flex-row h-10 justify-between items-center'>
-                    <Sortbar filters={filters} setFilters={setFilters} t={t} />
-                    <label className='h-10 text-[16px] text-[#99e24d]'>
-                     247 PROJECT
-                   </label>
-                  </div>
-                  <div
-                    className={`flex flex-row gap-2 rounded-lg p-4 shadow-lg ${
-                    isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
-                    }`}
-                  >                    
-                  <div className='flex items-center justify-center rounded-[10px] w-[50px] h-[50px] bg-white-100 bg-opacity-25'>
-                      <AiOutlineLike  size={30} color="#99e24d" />
+      <div className="flex w-full items-center justify-center">
+        <div className="flex w-[80%] flex-col">
+          <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+          <div className="flex">
+            <div className="flex w-[100%] flex-col items-start sm:flex-row ">
+              <Filter />
+              <div className="flex-1 p-4 ">
+                {filteredIssues.length === 0 ? (
+                  <p>{t('no-issues-found')}.</p>
+                ) : (
+                  <div className="flex flex-col gap-2">
+                    <div className="flex h-10 flex-row items-center justify-between">
+                      <Sortbar filters={filters} setFilters={setFilters} t={t} />
+                      <label className="h-10 text-[16px] text-[#99e24d]">247 PROJECT</label>
                     </div>
-                    <div className="flex flex-col w-full gap-0">
-                      <div className='flex w-full justify-between items-center'>
-                      <span className="text-[24px] text-white">Good first issues</span>
-                      <div className='flex items-center justify-center rounded-[20px] bg-[#99e24d] bg-opacity-30 w-[30px]'>
-                        <p className='text-[#99e24d]'>
-                        1
-                        </p>
+                    <div
+                      className={`flex flex-row gap-2 rounded-lg p-4 shadow-lg ${
+                        isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
+                      }`}
+                    >
+                      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-[10px] bg-white-100 bg-opacity-25">
+                        <AiOutlineLike size={30} color="#99e24d" />
+                      </div>
+                      <div className="flex w-full flex-col gap-0">
+                        <div className="flex w-full items-center justify-between">
+                          <span className="text-white text-[24px]">Good first issues</span>
+                          <div className="flex w-[30px] items-center justify-center rounded-[20px] bg-[#99e24d] bg-opacity-30">
+                            <p className="text-[#99e24d]">1</p>
+                          </div>
+                        </div>
+                        <div className="flex w-full">
+                          <p className="text-[16px]">
+                            Apply to a list of curated issues well suited for those new to the
+                            project to kickstart your journey.
+                          </p>
+                        </div>
                       </div>
                     </div>
-                    <div className='flex w-full'>
-                      <p className='text-[16px]'>
-                        Apply to a list of curated issues well suited for those new to the project to kickstart your journey.</p>
-                      </div>
+                    <div className="flex flex-col gap-4">
+                      {filteredIssues.map((issue) => (
+                        <IssueCard key={issue.github_id} issue={issue} t={t} />
+                      ))}
                     </div>
                   </div>
-                  <div className="flex flex-col gap-4">
-                    {filteredIssues.map((issue) => (
-                    <IssueCard key={issue.github_id} issue={issue} t={t} />
-                    ))}
-                  </div>
-                </div>
-              )}
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </div>
     </>
   )
