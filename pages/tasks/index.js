02768f17127b4678eb08bfafffae72ac60687252
Gustavo Boehm <gu.boehm@live.com>
2024-08-06 23:36:46 -0300

    fix: filter implementation

--- pages/tasks/index.js
+++ pages/tasks/index.js
@@ -1,75 +1,14 @@
 import React, { useEffect, useState } from 'react'
-import Head from 'next/head'
 import { withProtected } from '../../hooks/route'
 import { getAllTasks } from '../../lib/tasks'
 import { useTranslation } from 'react-i18next'
-import SearchBar from '../../components/SearchBar'
-import { MdGroup } from 'react-icons/md'
-import { AiOutlineLike } from 'react-icons/ai'
-import { Button, Image, Text } from '@nextui-org/react'
-import Filter from '../../components/FilterTasks/index'
-import Sortbar from '../../components/SortBar/index'
 import { useTheme } from 'next-themes'
-import Dropdown from '../../components/Dropdown'
-
-const IssueCard = ({ issue }) => {
-  const { t } = useTranslation()
-  const { theme } = useTheme()
-  const isLight = theme === 'light'
-  return (
-    <div
-      className={`flex md:flex-row flex-col items-center justify-center gap-2 rounded-lg p-4 shadow-lg ${
-        isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
-      }`}
-    >
-      <div className="flex h-[40px] md:h-full w-[40px] md:w-[80px] items-center justify-center md:rounded-[20px] rounded-[10px] bg-[#99e24d]">
-        <MdGroup size={70} color="white" />
-      </div>
-      <div className="mb-4 flex w-full flex-col gap-3">
-        <div className="flex w-full items-center justify-between">
-          <span className="md:text-[24px] text-[18px] text-[#99e24d]">{issue.title}</span>
-          <button className="text-white mr-2 md:mr-4 rounded bg-[#99e24d] bg-opacity-30 px-2 py-1 md:text-sm text-[10px] hover:bg-[#649e26] focus:outline-none focus:ring-2 focus:ring-[#99e24d]">
-            Apply
-          </button>
-        </div>
-        <div className="flex w-full">
-          <p className="text-[12px] md:text-[16px]">
-            {issue.body}
-          </p>
-        </div>
-        <div className="flex md:flex-row flex-col gap-3 text-gray-400">
-          <p className="text-[16px]">
-            <strong>Board:</strong> {issue.project_name}
-          </p>
-          <p className="text-[16px]">
-            <strong>Created At:</strong> {new Date(issue.createdAt).toLocaleDateString()}
-          </p>
-          <p className="text-[16px]">
-          <Dropdown title="Language" items={issue.language} />
-          </p>
-          <p className="text-[16px]">
-            <Dropdown title="Skill" items={issue.skill} />
-          </p>
-          <p className="text-[16px]">
-            <strong>Reward:</strong> {issue.reward}
-          </p>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const filtersTasks = {
-  skill: 'Skill',
-  effort: 'Effort',
-  reward: 'Reward'
-}
-
-const subItems = {
-  skill: ['Skill-1', 'Skill-2', 'Skill-3'],
-  effort: ['demanding Team', 'Context Depth'],
-  reward: []
-}
+import { AiOutlineLike } from 'react-icons/ai'
+import Head from 'next/head'
+import SearchBar from '../../components/SearchBar'
+import Filter from '../../components/Filter'
+import Sortbar from '../../components/SortBar'
+import IssueCard from '../../components/IssueCard'
 
 const TaskPage = ({ issues }) => {
   const { t } = useTranslation()
@@ -78,41 +17,57 @@ const TaskPage = ({ issues }) => {
   const [filters, setFilters] = useState({})
   const [searchQuery, setSearchQuery] = useState('')
 
+  const uniqueFields = {};
+
+  issues.forEach(issue => {
+    issue.fields.forEach(({ field, value }) => {
+      if (!uniqueFields[field]) {
+        uniqueFields[field] = new Set();
+      }
+      uniqueFields[field].add(value);
+    });
+  });
+
+  const filterOptions = Object.fromEntries(
+    Object.entries(uniqueFields).map(([field, values]) => [field, Array.from(values)])
+  );
+
   const filteredIssues = issues.filter((issue) => {
-    return !filters.status || issue.state === filters.status
-  })
+    return Object.keys(filters).every(filterKey => {
+      if (!filters[filterKey]) return true;
+      return issue.fields.some(field => field.field === filterKey && field.value === filters[filterKey]);
+    });
+  });
 
-   const [issueCount, setIssueCount] = useState(filteredIssues.length)
+  const [issueCount, setIssueCount] = useState(filteredIssues.length)
 
-   useEffect(() => {
-     setIssueCount(filteredIssues.length)
-   }, [filteredIssues])
+  useEffect(() => {
+    setIssueCount(filteredIssues.length)
+  }, [filteredIssues])
 
   return (
     <>
       <Head>
-        <title> Tasks - WEB3DEV</title>
+        <title>Tasks - WEB3DEV</title>
       </Head>
       <div className="flex w-full items-center justify-center">
         <div className="flex xl:w-[80%] w-full flex-col">
           <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
           <div className="flex">
             <div className="flex w-full flex-col items-start lg:flex-row md:mx-4">
-              <Filter filters={filtersTasks} subItems={subItems} />  
+              <Filter filters={filterOptions} selectedFilters={filters} setFilters={setFilters} />
               <div className="flex-1 p-2 ">
                 {filteredIssues.length === 0 ? (
                   <p>{t('no-issues-found')}.</p>
                 ) : (
                   <div className="flex flex-col gap-2">
                     <div className="flex h-10 flex-row items-center justify-between">
-                      <Sortbar filters={filters} setFilters={setFilters} t={t}/>
-                      <label className={`h-10 w-[80px] md:w-[100px] md:text-[16px] text-[10px] ${
-                        isLight ? 'text-black-400' : 'text-[#99e24d]'}`}>{filteredIssues.length} PROJECTS</label>
+                        <Sortbar filters={filters} setFilters={setFilters} t={t} />
+                        <label className={`h-10 w-[80px] md:w-[100px] md:text-[16px] text-[10px] ${isLight ? 'text-black-400' : 'text-[#99e24d]'}`}>{filteredIssues.length} PROJECTS</label>
                     </div>
                     <div
-                      className={`flex flex-row gap-2 rounded-lg p-2 shadow-lg ${
-                        isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
-                      }`}
+                        className={`flex flex-row gap-2 rounded-lg p-2 shadow-lg ${isLight ? 'bg-gray-200 bg-opacity-75' : 'bg-black-200 bg-opacity-75'
+                          }`}
                     >
                       <div className="flex h-[50px] w-[50px] items-center justify-center rounded-[10px] bg-white-100 bg-opacity-25">
                         <AiOutlineLike size={30} color="#99e24d" />
