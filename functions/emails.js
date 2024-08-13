458db4a9cb8f452c39207100b6284f02f7a2d3ea
Yan Luiz <yan@nomadbitcoin.xyz>
2024-08-13 10:40:50 -0300

    fix: update email nft email copy

--- functions/emails.js
+++ functions/emails.js
@@ -2,6 +2,7 @@ const nodemailer = require('nodemailer')
 const mandrillTransport = require('nodemailer-mandrill-transport')
 const { PubSub } = require('@google-cloud/pubsub')
 const pubsub = new PubSub()
+require('dotenv').config('../.env')
 
 const smtpTransport = nodemailer.createTransport(
   mandrillTransport({
@@ -17,17 +18,18 @@ async function sendEmail(template_name, subject, to, params = {}) {
   const template = require(`./email_templates/${template_name}`)
 
   const mailOptions = {
-    from: `danicuki <contato@${process.env.MANDRILL_DOMAIN}>`,
+    from: `Yan Luiz <contact@${process.env.MANDRILL_DOMAIN}>`,
     subject,
     html: template(params),
   }
 
+  console.log('sending email...')
   return await smtpTransport.sendMail({ ...mailOptions, to })
 }
 
 async function enqueueEmails(emails, template, subject, params) {
   for (to of emails) {
-    const messageObject = { to,template,subject,params }
+    const messageObject = { to, template, subject, params }
     const messageBuffer = Buffer.from(JSON.stringify(messageObject), 'utf8')
     pubsub.topic('course_day_email').publishMessage({ data: messageBuffer })
   }
