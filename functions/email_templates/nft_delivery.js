310989158edb020d6d2fccfdbb0be0a7761b9eb3
Yan Luiz <yan@nomadbitcoin.xyz>
2024-08-14 17:58:40 -0300

    chore: Portuguese text at email

--- functions/email_templates/nft_delivery.js
+++ functions/email_templates/nft_delivery.js
@@ -1,6 +1,8 @@
 function template(params) {
   // course_title, wallet_address, nft_contract, nft_id) {
   return `
+Note: Abaixo você encontrará mais informações em português.
+<br>
 Hello builder 👷👷‍♀️!<br>
 <br>
 You have just received your NFT for completing the bootcamp <b>${params.course_title}</b>! It's already in your wallet <a href='https://polygonscan.com/address/${params.wallet_address}#tokentxnsErc721'>${params.wallet_address}</a>.<br>
@@ -16,6 +18,24 @@ This helps us spread the word about our bootcamps to other people like you who w
 I'm happy with your certification, congratulations! See you at the next bootcamp!<br>
 <br>
 Cheers,<br>
+Yan Luiz<br>
+<br>
+<hr>
+Olá builder 👷👷‍♀️!<br>
+<br>
+Você acabou de receber seu NFT por completar o bootcamp <b>${params.course_title}</b>! Ele já está na sua carteira <a href='https://polygonscan.com/address/${params.wallet_address}#tokentxnsErc721'>${params.wallet_address}</a>.<br>
+<br>
+Você já pode ver seu NFT no <a href='https://opensea.io/assets/matic/${params.nft_contract}/${params.nft_id}?force_update=true'>OpenSea.</a><br>
+<br>
+Aproveite este momento para mostrar ao mundo o que você conquistou!!! Vá ao Twitter e marque <a href='https://twitter.com/web3dev_'>@web3dev_ </a>com a captura de tela do seu NFT, tenho certeza de que ficará ótimo!<br>
+<br>
+Veja <a href='https://pt.w3d.community/dione_b/utilizando-nfts-como-certificado-no-linkedin-31ce<br'>como adicionar seu certificado ao LinkedIn.</a><br>
+<br>
+Isso nos ajuda a divulgar nossos bootcamps para outras pessoas como você que querem aprender mais sobre Web3 :)<br>
+<br>
+Estou muito feliz com sua certificação, parabéns! Vejo você no próximo bootcamp!<br>
+<br>
+Grande Abraço,<br>
 Yan Luiz<br>
 <br>`
 }
