460a3e96e3e4f13ae846613ff58c9aa1d7ad9717
Gustavo Boehm <gu.boehm@live.com>
2024-08-15 18:48:34 -0300

    fix: personal data tests

--- components/Card/GeneralInfo/PersonalData/index.spec.js
+++ components/Card/GeneralInfo/PersonalData/index.spec.js
@@ -1,37 +1,45 @@
 /**
  * @jest-environment jsdom
  */
-import GeneralInfo from '../index'
+import GeneralInfoCard from '../index'
 import { fireEvent, render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { act } from 'react-dom/test-utils'
+import { SessionProvider } from 'next-auth/react'
 
 describe('Personal Data', () => {
   beforeEach(async () => {
-    render(<GeneralInfo />)
+    render(
+      <SessionProvider session={null}>
+        <GeneralInfoCard />
+      </SessionProvider>
+    )
   })
 
   it('should receive data input correctly', async () => {
-    fireEvent.input(screen.getByPlaceholderText('Nome'), {
+    const nameInput = screen.getByLabelText('Nome')
+    fireEvent.change(nameInput, {
       target: {
         value: 'Zé do alho',
       },
-    })
-    expect(screen.getByPlaceholderText('Nome')).toHaveValue('Zé do alho')
-    fireEvent.input(screen.getByPlaceholderText('Email'), {
+    });
+    expect(nameInput).toHaveValue('Zé do alho')
+
+    const emailInput = screen.getByLabelText('Email')
+    fireEvent.input(emailInput, {
       target: {
         value: 'exemplo@gmail.com',
       },
     })
-    expect(screen.getByPlaceholderText('Email')).toHaveValue('exemplo@gmail.com')
-    fireEvent.input(screen.getByPlaceholderText('Escreva um resumo sobre você'), {
+    expect(emailInput).toHaveValue('exemplo@gmail.com')
+
+    const bioInput = screen.getByLabelText('Descreva de maneira breve sua experiência com web3')
+    fireEvent.input(bioInput, {
       target: {
         value: 'Eu sou um desenvolvedor',
       },
     })
-    expect(screen.getByPlaceholderText('Escreva um resumo sobre você')).toHaveValue(
-      'Eu sou um desenvolvedor'
-    )
+    expect(bioInput).toHaveValue('Eu sou um desenvolvedor')
   })
 
   it('should throw yup error on saving without name', async () => {
@@ -39,55 +47,21 @@ describe('Personal Data', () => {
     await act(async () => {
       fireEvent.submit(screen.getByRole('button', { name: /salvar/i }))
     })
-    expect(screen.getByText('Nome é obrigatório')).toBeInTheDocument()
-  })
-
-  it('should throw yup error on saving name but without email', async () => {
-    fireEvent.input(screen.getByPlaceholderText('Nome'), {
-      target: {
-        value: 'Zé do alho',
-      },
-    })
-    expect(screen.getByPlaceholderText('Nome')).toHaveValue('Zé do alho')
-    expect(screen.getByRole('button', { name: /salvar/i }))
-    await act(async () => {
-      fireEvent.submit(screen.getByRole('button', { name: /salvar/i }))
-    })
-    expect(screen.getByText('Email é obrigatório')).toBeInTheDocument()
+    expect(screen.getAllByText('This field is required')[0]).toBeInTheDocument()
   })
 
   it('should throw yup error on saving email but without name', async () => {
-    fireEvent.input(screen.getByPlaceholderText('Email'), {
+    const emailInput = screen.getByLabelText('Email')
+    fireEvent.change(emailInput, {
       target: {
         value: 'exemplo@gmail.com',
       },
     })
-    expect(screen.getByPlaceholderText('Email')).toHaveValue('exemplo@gmail.com')
+    expect(emailInput).toHaveValue('exemplo@gmail.com')
     expect(screen.getByRole('button', { name: /salvar/i }))
     await act(async () => {
       fireEvent.submit(screen.getByRole('button', { name: /salvar/i }))
     })
-    expect(screen.getByText('Nome é obrigatório')).toBeInTheDocument()
-  })
-
-  it('should save profile data correctly if name and email are filled but bio is not ', async () => {
-    fireEvent.input(screen.getByPlaceholderText('Nome'), {
-      target: {
-        value: 'Zé do alho',
-      },
-    })
-    expect(screen.getByPlaceholderText('Nome')).toHaveValue('Zé do alho')
-    fireEvent.input(screen.getByPlaceholderText('Email'), {
-      target: {
-        value: 'exemplo@gmail.com',
-      },
-    })
-    expect(screen.getByPlaceholderText('Email')).toHaveValue('exemplo@gmail.com')
-    expect(screen.getByRole('button', { name: /salvar/i }))
-    await act(async () => {
-      fireEvent.submit(screen.getByRole('button', { name: /salvar/i }))
-    })
-    expect(screen.queryByText('Nome é obrigatório')).not.toBeInTheDocument()
-    expect(screen.queryByText('Email é obrigatório')).not.toBeInTheDocument()
+    expect(screen.getAllByText('This field is required')[0]).toBeInTheDocument()
   })
 })
