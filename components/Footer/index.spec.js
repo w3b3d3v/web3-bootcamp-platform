35c015aef6b7de39c07cb82220b9e9b0ade9d598
Gustavo Boehm <gu.boehm@live.com>
2024-08-15 14:52:43 -0300

    fix: footer tests

--- components/Footer/index.spec.js
+++ components/Footer/index.spec.js
@@ -4,17 +4,41 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Footer from './index'
+import { useRouter } from 'next/router'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}))
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => { }),
+      },
+    }
+  },
+}))
 
 describe('<Footer />', () => {
+  beforeEach(() => {
+    useRouter.mockImplementation(() => ({
+      pathname: '/courses',
+    }))
+  })
+
   it('should render Footer', () => {
     render(<Footer />)
     expect(screen.getByTestId('footer')).toBeInTheDocument()
   })
+
   it('should render web3dev link', () => {
     render(<Footer />)
     expect(screen.getByTestId('web3dev-link')).toBeInTheDocument()
   })
-  it('should render buildpsace link', () => {
+
+  it('should render buildspace link', () => {
     render(<Footer />)
     expect(screen.getByTestId('buildspace-link')).toBeInTheDocument()
   })
