4021fcd1827e692cb07b3f4dc1058772e21ef9fc
Gustavo Boehm <gu.boehm@live.com>
2024-08-15 16:11:12 -0300

    fix: functions path ignore  in jest.config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', ],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/', '<rootDir>/functions/'],
   // setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
   transform: {
     '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest',
