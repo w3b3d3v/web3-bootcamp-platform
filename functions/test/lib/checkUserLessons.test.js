41c9277be6f22df06e45ad0522cdced52ea262e3
Yan Luiz <yan@nomadbitcoin.xyz>
2024-08-15 23:39:39 -0300

    test:Refactored mock for 'initDb' module and fixed language in metadata for course tests.

--- functions/test/lib/checkUserLessons.test.js
+++ functions/test/lib/checkUserLessons.test.js
@@ -1,165 +1,104 @@
-const { userCompletedCourse, usersIdsCompletedBootcamp, usersToSend2ndChance } = require('../../lib/checkUserLessons')
-const admin = require('firebase-admin')
-admin.initializeApp()
+jest.mock('../../lib/initDb', () => ({
+  db: {
+    collection: jest.fn().mockReturnThis(),
+    where: jest.fn().mockReturnThis(),
+    get: jest.fn(),
+    doc: jest.fn().mockReturnThis(),
+  },
+}))
+
+const {
+  userCompletedCourse,
+  usersIdsCompletedBootcamp,
+  usersToSend2ndChance,
+} = require('../../lib/checkUserLessons')
+
+const { db } = require('../../lib/initDb')
 
 describe('Check User Lessons', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
   describe('userCompletedCourse', () => {
-    const db = admin.firestore()
-    let mockCollection, mockDoc, mockGet, mockWhere, mockData
-
-    beforeEach(() => {
-      // Reset mock functions before each test
-      mockCollection = jest.fn()
-      mockDoc = jest.fn()
-      mockGet = jest.fn()
-      mockWhere = jest.fn()
-      mockData = jest.fn()
-
-      db.collection = mockCollection
-      mockCollection.mockReturnValue({
-        doc: mockDoc,
-        where: mockWhere,
-      })
-      mockDoc.mockReturnValue({
-        get: mockGet,
-      })
-      mockWhere.mockReturnValue({
-        get: mockGet,
-      })
-    })
-
     it('should return false because the user has not completed the course', async () => {
-      const userId = 'hX5ELaRUTCd4ApsRQ2YJ0bJai1w2'
-      const courseId = 'Test'
+      const userId = 'testUser'
+      const courseId = 'testCourse'
 
-      mockGet.mockResolvedValueOnce({
-        docs: [
-          {
-            data: () => ({ lesson: 'Lesson_1_What_is_a_Smart_Contract.md' }),
-          },
-        ],
+      // Mock para lessons_submissions
+      db.get.mockResolvedValueOnce({
+        docs: [{ data: () => ({ lesson: 'Lesson_1_What_is_a_Smart_Contract.md' }) }],
       })
-      mockGet.mockResolvedValueOnce({
+
+      // Mock para o documento do curso
+      db.get.mockResolvedValueOnce({
         data: () => ({
-          sections: {
-            Section_0: [
-              { file: 'Lesson_1_What_is_a_Smart_Contract.md' },
-              { file: 'Lesson_2_Finalize_Celebrate.md' },
-            ],
+          metadata: {
+            en: {
+              sections: {
+                Section_0: [
+                  { file: 'Lesson_1_What_is_a_Smart_Contract.md' },
+                  { file: 'Lesson_2_Finalize_Celebrate.md' },
+                ],
+              },
+            },
           },
         }),
       })
 
-      const result = await userCompletedCourse(userId, courseId, db)
-
+      const result = await userCompletedCourse(userId, courseId)
       expect(result).toBe(false)
     })
 
     it('should return true if the user completed the course', async () => {
-      const userId = 'hX5ELaRUTCd4ApsRQ2YJ0bJai1w2'
-      const courseId = 'Test'
+      const userId = 'testUser'
+      const courseId = 'testCourse'
 
-      mockGet.mockResolvedValueOnce({
+      // Mock para lessons_submissions
+      db.get.mockResolvedValueOnce({
         docs: [
-          {
-            data: () => ({ lesson: 'Lesson_1_What_is_a_Smart_Contract.md' }),
-          },
-          {
-            data: () => ({ lesson: 'Lesson_2_Finalize_Celebrate.md' }),
-          },
+          { data: () => ({ lesson: 'Lesson_1_What_is_a_Smart_Contract.md' }) },
+          { data: () => ({ lesson: 'Lesson_2_Finalize_Celebrate.md' }) },
         ],
       })
-      mockGet.mockResolvedValueOnce({
+
+      // Mock para o documento do curso
+      db.get.mockResolvedValueOnce({
         data: () => ({
-          sections: {
-            Section_0: [
-              { file: 'Lesson_1_What_is_a_Smart_Contract.md' },
-              { file: 'Lesson_2_Finalize_Celebrate.md' },
-            ],
+          metadata: {
+            en: {
+              sections: {
+                Section_0: [
+                  { file: 'Lesson_1_What_is_a_Smart_Contract.md' },
+                  { file: 'Lesson_2_Finalize_Celebrate.md' },
+                ],
+              },
+            },
           },
         }),
       })
 
-      const result = await userCompletedCourse(userId, courseId, db)
-
+      const result = await userCompletedCourse(userId, courseId)
       expect(result).toBe(true)
     })
   })
 
   describe('usersIdsCompletedBootcamp', () => {
-    const db = admin.firestore()
-    let mockCollection, mockWhere, mockGet, mockData
-
-    beforeEach(() => {
-      // Reset mock functions before each test
-      mockCollection = jest.fn()
-      mockWhere = jest.fn()
-      mockGet = jest.fn()
-      mockData = jest.fn()
-
-      db.collection = mockCollection.mockReturnValue({
-        where: mockWhere,
-      })
-      mockWhere.mockReturnValue({
-        get: mockGet,
-      })
-    })
-
     it('should return an array of user IDs who completed the bootcamp', async () => {
-      const mockUserId1 = 'user123'
-      const mockUserId2 = 'user456'
-      mockGet.mockResolvedValueOnce({
-        docs: [
-          {
-            data: () => ({ user_id: mockUserId1 }),
-          },
-          {
-            data: () => ({ user_id: mockUserId2 }),
-          },
-        ],
-      })
+      db.get.mockResolvedValueOnce({ docs: [{ data: () => ({ user_id: 'user1' }) }] })
 
-      const result = await usersIdsCompletedBootcamp(db)
-
-      expect(result).toEqual([mockUserId1, mockUserId2])
+      const result = await usersIdsCompletedBootcamp()
+      expect(result).toEqual(['user1'])
     })
   })
 
   describe('usersToSend2ndChance', () => {
-    const db = admin.firestore()
-    let mockCollection, mockWhere, mockGet
-  
-    beforeEach(() => {
-        mockCollection = jest.fn()
-        mockWhere = jest.fn()
-        mockGet = jest.fn()
+    it('should include user in the second chance list if not completed bootcamp', async () => {
+      db.get.mockResolvedValueOnce({ docs: [] }) // Nenhum usuário completou o bootcamp
+      db.get.mockResolvedValueOnce({ docs: [{ data: () => ({ uid: 'user1' }) }] })
 
-        db.collection = mockCollection.mockReturnValue({
-            where: mockWhere,
-        })
-        mockWhere.mockReturnValue({
-            get: mockGet,
-        })
-    })
-
-    it('should include user in the second chance list if not completed bootcamp', async () => {  
-      
-      const mockFirestoreResponse = { 
-        docs: [
-          {
-            data: () => ({ uid: 'user1', user_id: 'user1'}, { uid: 'user23', user_id: 'user2' }),
-          }
-        ]
-      }
-      mockGet.mockResolvedValue(mockFirestoreResponse)
-  
-      const mockCohortId = 'cohort789'
-      const result = await usersToSend2ndChance(db, mockCohortId)
-
-      expect(mockCollection).toHaveBeenCalledWith('users')
-      expect(mockWhere).toHaveBeenCalledWith('cohort_ids', 'array-contains', mockCohortId)
-      expect(mockGet).toHaveBeenCalled()
-      expect(result).toContainEqual({ uid: 'user23', 'user_id': 'user2' })
+      const result = await usersToSend2ndChance('cohortId')
+      expect(result).toEqual([{ uid: 'user1' }])
     })
   })
 })
