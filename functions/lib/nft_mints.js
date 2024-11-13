d6b9449bb273d76a6d125b0003f67c186d01a116
Yan Luiz <yan@nomadbitcoin.xyz>
2024-11-13 14:25:23 -0300

    chore: get missing nft mints from all cohorts

--- functions/lib/nft_mints.js
+++ functions/lib/nft_mints.js
@@ -7,7 +7,7 @@ exports.getMissingNfts = async function () {
     join web3dev_bootcamp.cohorts c2 on c.course_id = c2.course_id 
     join web3dev_bootcamp.users u on u.id = l.user_id
     left join web3dev_bootcamp.nft_mints n on n.user_id = l.user_id and (n.cohort_id = l.cohort_id or n.cohort_id = c2.id)
-      where (lesson = 'Lesson_2_Finalize_Celebrate.md' or lesson = 'Lesson_2_Ship_It.md' or lesson = 'Lesson_2_Finalize_And_Celebrate.md' or lesson = 'Lesson_2_Finishing_Touches_Contract.md' or lesson = 'Lesson_5_Course_Completed.md')
+      where (lesson = 'Lesson_2_Finalize_Celebrate.md' or lesson = 'Lesson_2_Ship_It.md' or lesson = 'Lesson_3_All_Done.md' or lesson = 'Lesson_2_Finalize_And_Celebrate.md' or lesson = 'Lesson_1_Finishing_touches.md' or lesson = 'Lesson_5_Course_Completed.md')
     and n.user_id is null
     and u.wallet is not null`
   return await getQueryResults(query)
